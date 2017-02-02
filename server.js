9c168d0977f8a3cc6fa446ce0144f68bbe47d35a
Eric Ewe <eric.ewe@zalora.com>
2017-02-02 11:48:49 +0800

    Create server and update package.json

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.listen(port);
+
+console.log("Running at Port 3000");
