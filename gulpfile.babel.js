4dea1ba0151aa0006a5b8f567abacfe22ebac27c
Eric Ewe <eric.ewe@zalora.com>
2017-02-03 16:33:10 +0800

    Enable bundling automation with gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,66 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const csso = require('gulp-csso');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const eslint = require('gulp-eslint');
+const nodemon = require('nodemon');
+const webpack = require('webpack-stream');
+
+const webpackConfig = require('./webpack.config.js');
+const rootDir = './dist';
+
+require('babel-core/register');
+require('babel-polyfill');
+
+/** lint task **/
+gulp.task('lint', () =>
+  gulp.src(['./app/**/*.js', '!node_modules/**', '!./app/resources/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.formatEach())
+    .pipe(eslint.failAfterError())
+);
+
+/** minimizing files and bundling **/
+gulp.task('css', () =>
+  gulp.src('./app/resources/style/**/*.scss')
+    .pipe(
+      sass({
+        includePaths: ['./app/resources/style'],
+        errLogToConsole: true,
+      }))
+    .pipe(csso())
+    .pipe(gulp.dest(`${rootDir}/resources/style/`))
+);
+
+gulp.task('html', () =>
+  gulp.src('./app/*.html')
+    .pipe(gulp.dest(rootDir))
+);
+
+gulp.task('image', () =>
+  gulp.src('./app/resources/images/*.+(png|jpg|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest(`${rootDir}/resources/images/`))
+);
+
+gulp.task('bundle', () =>
+  gulp.src('./app/app.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(rootDir))
+);
+
+/** watch file changes **/
+gulp.task('watch', () => {
+  gulp.watch('./app/resources/style/**/*.scss', ['css']);
+  gulp.watch('./app/*.html', ['html']);
+  gulp.watch('./app/**/*.js', ['bundle']);
+});
+
+/** run gulp task for development **/
+gulp.task('default', ['watch', 'css', 'html', 'image', 'bundle'], () => {
+  nodemon({
+    script: './server.js',
+    ignore: ['./dist/'],
+  });
+});
