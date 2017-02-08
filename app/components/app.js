9304d9b3d8729c7547d9434d6f44bf888aefb20e
Eric Ewe <eric.ewe@zalora.com>
2017-02-08 15:03:24 +0800

    Add a header onto app shell

--- app/components/app.js
+++ app/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 class App extends React.Component {
   render() {
     return(
     <div>
+      <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top">
+        <Link to="/"><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
+      </nav>
       <div id="app-body">
         {this.props.children}
       </div>
