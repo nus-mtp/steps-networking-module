adc73a0084cbb94762f3469e7e50af1921a93c7e
Eric Ewe <eric.ewe@zalora.com>
2017-02-09 11:53:17 +0800

    Lint files

--- app/components/app.js
+++ app/components/app.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 
-class App extends React.Component {
-  render() {
-    return(
-    <div>
-      <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top">
-        <Link to="/"><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
-      </nav>
-      <div id="app-body">
-        {this.props.children}
-      </div>
+const App = ({ children }) => (
+  <div>
+    <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top">
+      <Link to="/"><img id="brand-logo" src="resources/images/home.svg" alt="Home" /></Link>
+    </nav>
+    <div id="app-body">
+      {children}
     </div>
-    );
-  }
-}
+  </div>
+);
+
+App.propTypes = {
+  children: React.PropTypes.element.isRequired,
+};
 
 export default App;
