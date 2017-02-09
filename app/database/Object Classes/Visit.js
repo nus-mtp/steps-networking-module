9123bf3886bab1e8e870d4a6edbe3cce40046920
Jean <jkohx3@gmail.com>
2017-02-09 17:02:05 +0800

    Revert "Created Visit Class attributes"
    This reverts commit 49d34bf8b0f97fae2b4ac521d18517e4070dcaa0.

--- app/database/Object Classes/Visit.js
+++
@@ -1,6 +0,0 @@
-function Visit(UserEmail, EventID, Reason) {
-    var TicketID = 0; //dummy value. to be auto-generated
-    this.UserEmail = UserEmail;
-    this.EventID = EventID;
-    this.reason = Reason
-}
