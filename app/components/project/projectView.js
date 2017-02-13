7816070e68760568d92379375f6409609446ce89
Muhammad Adam <splitfire_1992@hotmail.com>
2017-02-13 16:48:30 +0800

    Created template for Exhibition Page (unstylized)

--- app/components/project/projectView.js
+++ app/components/project/projectView.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const ProjectView = () => (
-  <div id="project-body" />
-);
+class ProjectView extends React.Component {
+  render() {
+    return (
+      <div id="project-body">
+
+        <div className="row justify-content-center">
+          <div id="project-poster">
+            <img  src="https://goo.gl/uYtfxK" alt="project-poster" />
+          </div>
+        </div>
+
+        <div className="exhibition-info">
+          <div className="info-type">Project Title</div>
+          <div id="project-name" className="project-info" />
+          <div className="info-type">Description</div>
+          <div id="project-desc" className="project-info" />
+          <div className="info-type">Tags</div>
+          <div id="project-tags" className="project-info" />
+        </div>
+
+        <div className="exhibition-info">
+          <div className="info-type">Related Media / URLs</div>
+          <div id="project-url" />
+          <div id="project-media" />
+        </div>
+
+        <div className="exhibition-info">
+          <div className="info-type">Project Members</div>
+          <div id="project-members" />
+        </div>
+
+        
+
+      </div>
+    );
+  }
+}
 
 export default ProjectView;
