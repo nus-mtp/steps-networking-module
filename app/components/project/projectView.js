8089523c297b5df57747d4ddac4ee44c9510f1f9
Muhammad Adam <splitfire_1992@hotmail.com>
2017-02-13 19:58:26 +0800

    Included comment section

--- app/components/project/projectView.js
+++ app/components/project/projectView.js
@@ -7,31 +7,40 @@ class ProjectView extends React.Component {
 
         <div className="row justify-content-center">
           <div id="project-poster">
-            <img  src="https://goo.gl/uYtfxK" alt="project-poster" />
+            <img src="../../resources/images/dummy-poster.png" alt="project-poster" />
           </div>
         </div>
 
         <div className="exhibition-info">
-          <div className="info-type">Project Title</div>
+          <div className="info-type">Project Title: </div>
           <div id="project-name" className="project-info" />
-          <div className="info-type">Description</div>
+          <div className="info-type">Description: </div>
           <div id="project-desc" className="project-info" />
-          <div className="info-type">Tags</div>
+          <div className="info-type">Tags: </div>
           <div id="project-tags" className="project-info" />
         </div>
 
-        <div className="exhibition-info">
+        <div className="exhibition-info text-center">
           <div className="info-type">Related Media / URLs</div>
           <div id="project-url" />
           <div id="project-media" />
         </div>
 
-        <div className="exhibition-info">
+        <div className="exhibition-info text-center">
           <div className="info-type">Project Members</div>
           <div id="project-members" />
+
+          <button id="broadcast-msg" type="button" className="btn btn-info">Broadcast Message</button>
         </div>
 
-        
+        <div className="exhibition-info text-center">
+          <div className="info-type">Comments</div>
+          <div id="project-comments" />
+          <div className="form-group">
+            <textarea className="form-control" rows="1" id="comment"></textarea>
+          </div>
+          <button id="submit-comment" type="button" className="btn btn-primary">Submit</button>
+        </div>
 
       </div>
     );
