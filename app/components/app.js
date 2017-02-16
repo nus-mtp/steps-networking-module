bf42c5dfb5b32b065c462a825583b84715d2da95
Eric Ewe <eric.ewe@zalora.com>
2017-02-16 17:39:42 +0800

    Add login and register links

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import Auth from '../database/auth';
 
 class App extends React.Component {
   render() {
     return(
     <div>
-      <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top">
+      <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top flex-row">
         <Link to="/"><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
+        <div id="user-authentication">
+          { Auth.isUserAuthenticated() ?
+            <Link to="/logout">Log out</Link> : <div><Link to="/login">Log in</Link><Link to="/signup">Sign Up</Link></div>
+          }
+        </div>
       </nav>
       <div id="app-body">
         {this.props.children}
