ae4bc9efa26008f10417efb0cc95199c8b407431
Sean <sean@u.nus.edu>
2017-02-16 18:56:37 +0800

    Added Mongoose version of createDocuments.js.

---
+++ app/database/mongodbscripts/createDocumentsMongoose.js
@@ -0,0 +1,56 @@
+/*
+    This file contains the Schema for Document Creation in our Database.
+*/
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+/* 
+    Schema Definition 
+*/
+
+var visitSchema = new Schema({
+    event_id: Schema.Types.ObjectId,
+    reason: String
+});
+
+var participateSchema = new Schema({
+    exhibition_id: Schema.Types.ObjectId,
+    reason: String
+});
+
+var userSchema = new Schema({
+    email: String,
+    name: String,
+    description: String,
+    hashed_pw: String,
+
+    will_notify: Boolean,
+    is_deleted: Boolean,
+
+    events_visited: [visitSchema],
+    events_participated: [participateSchema]
+});
+
+var eventSchema = new Schema({
+    event_name: String,
+    event_description: String,
+    start_date: Date,
+    end_date: Date,
+    event_location: String,
+    event_map: String,
+    event_picture: String,
+
+    exhibitions_hosted: [Schema.Types.ObjectId],
+    tags: [String]
+});
+
+/* 
+    Model Definition
+*/
+
+var Visit = mongoose.model('visit', visitSchema);
+var Participate = mongoose.model('participate', participateSchema);
+var User = mongoose.model('user', userSchema);
+var Event = mongoose.model('event', eventSchema);
