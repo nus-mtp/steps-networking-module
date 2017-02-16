018a4e1a9018fcefed7db214a9bb8ea3e23ab92a
Jean <jkohx3@gmail.com>
2017-02-16 11:26:12 +0800

    removal of old files from a renamed folder.

--- app/database/Object Classes/User.js
+++
@@ -1,8 +0,0 @@
-function User(Name, Email, Description, Hashed_PW, isNotified, isDeleted) {
-    this.Name = Name;
-    this.Email = Email;
-    this.Description = Description;
-    this.Hashed_PW = Hashed_PW;
-    this.isNotfied = isNotified;
-    this.isDeleted = isDeleted;
-}
