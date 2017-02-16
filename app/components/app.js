aadf8025f985f88b85b1f94c27f911bf82275e59
Eric Ewe <eric.ewe@zalora.com>
2017-02-16 18:10:19 +0800

    Style authentication buttons

--- app/components/app.js
+++ app/components/app.js
@@ -8,9 +8,13 @@ class App extends React.Component {
     <div>
       <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top flex-row">
         <Link to="/"><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
-        <div id="user-authentication">
+        <div id="user-authentication-buttons" className="justify-content-end d-flex">
           { Auth.isUserAuthenticated() ?
-            <Link to="/logout">Log out</Link> : <div><Link to="/login">Log in</Link><Link to="/signup">Sign Up</Link></div>
+            <Link to="/logout"><button id="logout" type="button" className="btn btn-secondary">Logout</button></Link> :
+            <div>
+              <Link to="/login"><button id="login" type="button" className="btn-sm btn-primary">Login</button></Link>
+              <Link to="/signup"><button id="signup" type="button" className="btn-sm btn-secondary">Sign Up</button></Link>
+            </div>
           }
         </div>
       </nav>
