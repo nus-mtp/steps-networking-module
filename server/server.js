c80dbe724008d8e2f45755dc565bd48984d60ab5
Eric Ewe <eric.ewe@zalora.com>
2017-02-16 17:38:00 +0800

    Edit server structure and remove redundant code

--- server/server.js
+++ server/server.js
@@ -1,80 +1,41 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
 var routes = require('./routes/index');
+var config = require('./config.json');
+
+require('./models').connect(config.dbUri);
 
 var app = express();
 var port = 3000;
 
-app.use(express.static(__dirname + '../dist'));
-app.use('/css', express.static(__dirname + '../node_modules/bootstrap/dist/css'));  // redirect CSS bootstrap
+app.use(express.static(path.join(__dirname, '/../dist')));
+app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));  // redirect CSS bootstrap
 
 app.get('/',function(req,res){
   res.sendFile('index.html');   //It will find and locate index.html from View or Scripts
 });
 
-app.set('view engine', 'html');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, '../dist')));
 
+var localSignUpStrategy = require('./passport/local-signup');
+var localLoginStrategy = require('./passport/local-login');
+passport.use('local-signup', localSignUpStrategy);
+passport.use('local-login', localLoginStrategy);
+// app.use(passport.session());
 
-app.use('/', routes);
+const authCheckMiddleware = require('./middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
-// passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// mongoose
-mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+var authRoutes = require('./routes/auth');
+var apiRoutes = require('./routes/api');
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(port);
 
