136707ad80b621fac1a30a36832b35f0b9621fde
Sean <sean@u.nus.edu>
2017-02-16 15:01:03 +0800

    Removed old files.

--- app/database/mongodbscripts/insertuser.js
+++
@@ -1,9 +0,0 @@
-// Note 1: Ensure the accessmongodb_fixture.js is in the same directory as this script
-
-/*
-user : [
-	{ email : "user1@user.com", name : "user1", hashed_pw : "234632", description : "", willnotify : false, is_deleted : false, visits : [], profile_pic_url : ""},
-	{ email : "user2@user.com", name : "user2", hashed_pw : "234422", description : "", willnotify : false, is_deleted : false, visits : [], profile_pic_url : ""},
-	{ email : "user2@user.com", name : "user2", hashed_pw : "234422", description : "", willnotify : false, is_deleted : false, visits : [], profile_pic_url : ""}
-]
-*/
