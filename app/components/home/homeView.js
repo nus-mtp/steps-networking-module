2fbc45ef4853bccde786b3adfedbe51b48edb8f8
Muhammad-Adam <splitfire_1992@hotmail.com>
2017-02-16 16:59:29 +0800

    Partial Template of Event View
    Tabs and Events.

--- app/components/home/homeView.js
+++ app/components/home/homeView.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
-const HomeView = () => (
-  <div id="home-body" />
-);
+class HomeView extends React.Component {
+  render() {
+    return (
+      <div className="home-body">
+        <Tabs />
+        <div className="row events-section justify-content-center">
+          <Event />
+          <Event />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Tabs extends React.Component {
+  render() {
+    return (
+      <div className="event-tabs">
+        <ul className="nav nav-tabs justify-content-around">
+          <Tab name="Ongoing" />
+          <Tab name="Upcoming" />
+          <Tab name="Past" />
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Tab extends React.Component {
+  render() {
+    return (
+      <div className="event-tab">
+        <li className="nav-item">
+          <a className="nav-link" href="#">{this.props.name}</a>
+        </li>
+      </div>
+    );
+  }
+}
+
+class Event extends React.Component {
+  handleClick(event) {
+    console.log("Hello");
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick.bind(this)} className="event-picture">
+        <img className="img-fluid event-poster" src="https://goo.gl/T2k8fq" alt="Event picture" />
+        <div id="event-id" />
+      </div>
+    );
+  }
+}
 
 export default HomeView;
