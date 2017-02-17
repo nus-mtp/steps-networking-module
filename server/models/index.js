79cf580632b00a3076ef83bec6ea2cff118893ee
Eric Ewe <eric.ewe@zalora.com>
2017-02-17 21:08:21 +0800

    Change db settings

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = (uri) => {
-  //mongoose.connect(uri);
+  mongoose.connect(uri);
 
-  //mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise;
 
-  /*
+
   mongoose.connection.on('error', (err) => {
     console.error(`Mongoose connection error: ${err}`);
     process.exit(1);
   });
-  */
+  
   require('./user');
 }
