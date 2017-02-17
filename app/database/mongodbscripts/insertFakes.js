e0c9f57ac2ebf4005b8e5c3677a7ce5ecb428d76
Sean <sean@u.nus.edu>
2017-02-17 09:44:28 +0800

    Changed insertFakes.js's code for schemas.js and accessFakeDataMongoDB.js.

--- app/database/mongodbscripts/insertFakes.js
+++ app/database/mongodbscripts/insertFakes.js
@@ -1,41 +1,31 @@
 /*
-    This file contains a script to populate the database with documents.
-    Ensure that a MongoDB local server connection is up and running before executing.
+    This file contains a script to populate the fake-data database with documents.
+    Ensure that a MongoDB local server connection is running before executing.
 */
 
-var createDocuments = require("./createDocumentsMongoose.js");
+var db = require("./accessFakeDataMongoDB.js");
+var schemas = require("./schemas.js");
 
-var mongoose = require("mongoose");
+// Removes all prior documents created in the previous run
 
-var db;
+// Defines the Models for our Database
 
-mongoose.connect("mongodb://localhost:27017/fake-data");
+var User = db.model('user', schemas.userSchema);
+var Event = db.model('event', schemas.eventSchema);
 
-db = mongoose.connection;
+// Generate 5 test users
 
-db.on('error', console.error.bind(console, "Connection Error: "));
+var user1 = new User({ email: "user1@user.com", name: "user1", description: "I am user1.", hashed_pw: "" });
+var user2 = new User({ email: "user2@user.com", name: "user2", description: "I am user2.", hashed_pw: "" });
+var user3 = new User({ email: "user3@user.com", name: "user3", description: "I am user3.", hashed_pw: "" });
+var user4 = new User({ email: "user4@user.com", name: "user4", description: "I am user4.", hashed_pw: "" });
+var user5 = new User({ email: "user5@user.com", name: "user5", description: "I am user5.", hashed_pw: "" });
 
-db.collections["users"].drop(function(err) {
+var userArray = [user1, user2, user3, user4, user5];
+
+User.collection.insert(userArray, function(err, result) {
     if (err) console.log(err);
-});
-
-db.once('open', function() {
-
-    var User = createDocuments.User;
-
-    // Generate and Insert Users
-    var user1 = new User({ email: "user1@user.com", name: "user1", description: "I am user1.", hashed_pw: "" });
-    var user2 = new User({ email: "user2@user.com", name: "user2", description: "I am user2.", hashed_pw: "" });
-    var user3 = new User({ email: "user3@user.com", name: "user3", description: "I am user3.", hashed_pw: "" });
-    var user4 = new User({ email: "user4@user.com", name: "user4", description: "I am user4.", hashed_pw: "" });
-    var user5 = new User({ email: "user5@user.com", name: "user5", description: "I am user5.", hashed_pw: "" });
-
-    var userArray = [user1, user2, user3, user4, user5];
-
-    User.collection.insert(userArray, function(err, result) {
-        if (err) console.log(err);
-
-        mongoose.disconnect();
-    });
 
 });
+
+db.close(); // May be problematic - alternative is mongoose.disconnect();
