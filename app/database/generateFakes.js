a2045bbbb0533c92634f136ab21e35cbcae0fe85
Sean <sean@u.nus.edu>
2017-02-18 13:50:35 +0800

    Created fake database objects.

---
+++ app/database/generateFakes.js
@@ -0,0 +1,74 @@
+var user1 = {
+    _id: "2384239",
+    email: "user1@user.com",
+    name: "user1",
+    description: "I am user1.",
+    password: "$2a$06$8Syy6cDw6Ncsdp7t2pCL2ORsuUKhmGTi2s8hEJGuDUfGHmEn1GK86", // user1
+
+    will_notify: false,
+    is_deleted: false,
+    profile_picture: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1",
+
+    events_visited: [],
+    events_participated: [],
+};
+
+var user2 = {
+    _id: "wiuei238",
+    email: "user2@user.com",
+    name: "user2",
+    description: "I am user2.",
+    password: "$2a$06$/rmJ6ttximt9.q7jR36Jce1v5jfe8MW2XCThfY1xEDzAUBEOtwAly", // user2
+
+    will_notify: false,
+    is_deleted: false,
+    profile_picture: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1",
+
+    events_visited: [],
+    events_participated: [],
+};
+
+var user3 = {
+    _id: "238qfqf9",
+    email: "user3@user.com",
+    name: "user3",
+    description: "I am user3.",
+    password: "$2a$06$8lm19BOkfshp/7q6mJSLSebtjSBDFHoMopoVzKIDBIXvvwBh8H80S", // user3
+
+    will_notify: false,
+    is_deleted: false,
+    profile_picture: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1",
+
+    events_visited: [],
+    events_participated: [],
+};
+
+var user4 = {
+    _id: "23rrr2339",
+    email: "user4@user.com",
+    name: "user4",
+    description: "I am user4.",
+    password: "$2a$06$DLN.ULy2BGB0VgJX3lwNge5bWvE48n0Sra94p4KSmW2rqoFJX0M7W", // user4
+
+    will_notify: false,
+    is_deleted: false,
+    profile_picture: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1",
+
+    events_visited: [],
+    events_participated: [],
+};
+
+var user5 = {
+    _id: "2384239",
+    email: "user1@user.com",
+    name: "user1",
+    description: "I am user1.",
+    password: "$2a$06$QJuXZzeRxTqhUc5uCHHoS.agkkgNGhk2zTx6MtxOZzXpTbFRBvMbO",
+
+    will_notify: false,
+    is_deleted: false,
+    profile_picture: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1",
+
+    events_visited: [],
+    events_participated: [],
+};
