829f3e37665213f691db232fdda43e9d1b25fb78
Sean <sean@u.nus.edu>
2017-02-18 13:01:42 +0800

    Generalized the connection header function.

---
+++ app/database/mongodbscripts/accessMongoDB.js
@@ -0,0 +1,25 @@
+/*
+    This file defines a function that returns a connection to a specified MongoDB database upon execution.
+    Ensure that a MongoDB local server connection is running before executing.
+
+    Note that running this script by itself will not terminate the process.
+*/
+
+/*
+
+*/
+module.exports.connect = function(host, port, database) {
+    var mongoose = require('mongoose');
+
+    var db = mongoose.createConnection('mongodb://' + host + ':' + port + '/' + database);
+
+    db.on('error', function(err) {
+        if (err) throw err;
+    });
+
+    db.once('open', function() {
+        console.info('MongoDB ' + host + ' Connected Successfully.');
+    });
+
+    return db;
+}
