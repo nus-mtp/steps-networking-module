0d1f3fb0c5c46e2b8bc62aa98a7ff20885e13dc9
Muhammad-Adam <splitfire_1992@hotmail.com>
2017-02-21 13:51:06 +0800

    2 events share the same collapsable box

--- app/components/home/homeView.js
+++ app/components/home/homeView.js
@@ -6,8 +6,9 @@ class HomeView extends React.Component {
       <div className="home-body">
         <Tabs />
         <div className="row events-section justify-content-center">
-          <Event />
-          <Event />
+          <Event serial="1" />
+          <Event serial="1" />
+          <Collapsable serial="1" />
         </div>
       </div>
     );
@@ -41,15 +42,31 @@ class Tab extends React.Component {
 }
 
 class Event extends React.Component {
-  handleClick(event) {
-    console.log("Hello");
-  }
-
   render() {
     return (
-      <div onClick={this.handleClick.bind(this)} className="event-picture">
-        <img className="img-fluid event-poster" src="https://goo.gl/T2k8fq" alt="Event picture" />
-        <div id="event-id" />
+      <div>
+        <div onClick={this.handleClick} className="event-picture" data-toggle="collapse" href={"#collapseExample"+this.props.serial}
+          aria-expanded="false" aria-controls={"collapseExample"+this.props.serial}>
+          <img className="img-fluid text-center event-poster" src="https://goo.gl/T2k8fq" alt="Event picture" />
+          <div id="event-id" />
+        </div>
+    </div>
+    );
+  }
+}
+
+class Collapsable extends React.Component {
+  render() {
+    return (
+      <div className="collapse" id={"collapseExample"+this.props.serial}>
+        <div className="card card-block">
+          <div>
+            <h2>Title</h2>
+            <div id="event-title" />
+            <h4>Description</h4>
+            <div id="event-description" />
+          </div>
+        </div>
       </div>
     );
   }
