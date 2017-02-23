f2d4b92cf00278070c98674f64a5bab099bf551c
Eric Ewe <eric.ewe@zalora.com>
2017-02-23 19:06:46 +0800

    Restructure projectView layout

--- app/components/project/projectView.js
+++ app/components/project/projectView.js
@@ -6,42 +6,44 @@ class ProjectView extends React.Component {
       <div id="project-body">
 
         <div className="row justify-content-center">
-          <div id="project-poster">
-            <img className="img-fluid" src="../../resources/images/dummy-poster.png" alt="project-poster" />
+          <div>
+            <img id="project-poster" className="img-fluid" src="../../resources/images/dummy-poster.png" alt="project-poster" />
           </div>
         </div>
+				
+				<div className="card">
+					<div className="exhibition-info card-block">
+						<div className="info-type">Project Title: </div>
+						<div id="project-name" className="project-info" />
+						<div className="info-type">Description: </div>
+						<div id="project-desc" className="project-info" />
+						<div className="info-type">Tags: </div>
+						<div id="project-tags" className="project-info" />
+					</div>
+					<ul className="list-group list-group-flush">
+						<li className="exhibition-info text-center list-group-item">
+							<div className="info-type">Related Media / URLs</div>
+							<div id="project-url" />
+							<div id="project-media" />
+						</li>
 
-        <div className="exhibition-info">
-          <div className="info-type">Project Title: </div>
-          <div id="project-name" className="project-info" />
-          <div className="info-type">Description: </div>
-          <div id="project-desc" className="project-info" />
-          <div className="info-type">Tags: </div>
-          <div id="project-tags" className="project-info" />
-        </div>
+						<li className="exhibition-info text-center list-group-item">
+							<div className="info-type">Project Members</div>
+							<div id="project-members" />
 
-        <div className="exhibition-info text-center">
-          <div className="info-type">Related Media / URLs</div>
-          <div id="project-url" />
-          <div id="project-media" />
-        </div>
+							<button id="broadcast-msg" type="button" className="btn btn-info">Broadcast Message</button>
+						</li>
 
-        <div className="exhibition-info text-center">
-          <div className="info-type">Project Members</div>
-          <div id="project-members" />
-
-          <button id="broadcast-msg" type="button" className="btn btn-info">Broadcast Message</button>
-        </div>
-
-        <div className="exhibition-info text-center">
-          <div className="info-type">Comments</div>
-          <div id="project-comments" />
-          <div className="form-group">
-            <textarea className="form-control" rows="1" id="comment"></textarea>
-          </div>
-          <button id="submit-comment" type="button" className="btn btn-primary">Submit</button>
-        </div>
-
+						<li className="exhibition-info text-center card-block list-group-item">
+							<div className="info-type">Comments</div>
+							<div id="project-comments" />
+							<div>
+								<textarea className="form-control" rows="1" id="comment"></textarea>
+							</div>
+							<button id="submit-comment" type="button" className="btn btn-primary">Submit</button>
+						</li>
+					</ul>
+				</div>
       </div>
     );
   }
