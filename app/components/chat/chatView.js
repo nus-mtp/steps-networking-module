f941389b8ee98c275c536c69fb8aa2b7fd9dce46
Vylantze <digi9346@hotmail.com>
2017-02-23 19:35:50 +0800

    Fixed bug where word-wrap does not wrap. Fixed linting in chatView.js.

--- app/components/chat/chatView.js
+++ app/components/chat/chatView.js
@@ -1,97 +1,104 @@
 import React, { Component } from 'react';
 
 export default class ChatView extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			messages: [
-				this.Chat_postSelf('Turkey'),
-				this.Chat_postOther('Bacon'),
-				this.Chat_postSelf('Test')
-			]
-		};
-		
-		this.placeholder = 'Type a message...';
-	}
-	
-	addMessages(text) {
-		var messages = this.state.messages.slice();
-		messages.push(text);
-		this.setState({messages: messages});
-	}
-	
-	Chat_postSelf (text) {
-		return (
-			<div className="container form-control" id="chat-self">
-				{text}
-			</div>
-		);
-	}
+  static PostSelf(text) {
+    return (
+      <div className="container form-control" id="chat-self">
+        {text}
+      </div>
+    );
+  }
 
-	Chat_postOther (text) {
-		return (
-			<div className="container-fluid form-control" id="chat-other">
-				{text}
-			</div>
-		);
-	}
-	
-	handleChange (e) {
-		if (this.refs.chatinput.value = '\n') {
-			this.refs.chatinput.value = '';
-		}
-	}
+  static PostOther(text) {
+    return (
+      <div className="container-fluid form-control" id="chat-other">
+        {text}
+      </div>
+    );
+  }
 
-	handleSubmit (e) {
-		var str = this.refs.chatinput.value;
-		var str_strip = str.trim();
-		if (str_strip.length>0) {
-			var new_div = this.Chat_postSelf(this.refs.chatinput.value);
-			this.addMessages(new_div);
-			this.refs.chatinput.value = '';
-		} else {
-			this.refs.chatinput.value = str.slice(0, str.length-1);
-		}
-	}
-	
-	catchSubmit (e) {
-		e = e || event;
-		if (e.keyCode==13 && !e.shiftKey) {
-			e.preventDefault();
-			this.handleSubmit (e);
-		}
-		return true;
-	}
-	
-	scrollToBottom () {
-		//window.scrollTo(1, 1)
-		document.body.scrollTop = document.body.scrollHeight;
-	}
-	
-	componentDidUpdate () {
-		this.scrollToBottom();
-	}
-	
-	render() {
-		return (
-			<div className="container" id="chat-body">
-				<div className="container-fluid" id="chat-content-container">
-					{this.state.messages}
-				</div>
-				<div className="fixed-bottom" id="chat-form-container">
-					<textarea
-						className="form-control"  
-						ref="chatinput"
-						id="chat-input"
-						placeholder={this.placeholder}
-						onKeyDown={this.catchSubmit.bind(this)}
-					/>
-					<button type="button" id="chat-submit-button" className="btn btn-default" 
-						onClick={this.handleSubmit.bind(this)}><img id="chat-submit-image" src="../../resources/images/ic_send_24dp.png" />
-					</button>
-				</div>
-			</div>
-		);
-	}
-}//*/
+  static scrollToBottom() {
+    document.body.scrollTop = document.body.scrollHeight;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [
+        ChatView.PostSelf('Turkey'),
+        ChatView.PostOther('Bacon'),
+        ChatView.PostSelf('Test'),
+      ],
+    };
+
+    this.placeholder = 'Type a message...';
+  }
+
+  componentDidUpdate() {
+    ChatView.scrollToBottom();
+  }
+
+  addMessages(text) {
+    const messages = this.state.messages.slice();
+    messages.push(text);
+    this.setState({ messages }); // Replace the messages inside state
+  }
+
+  handleChange() {
+    if (this.refs.chatinput.value === '\n') {
+      this.refs.chatinput.value = '';
+    }
+  }
+
+  handleSubmit() {
+    const str = this.refs.chatinput.value;
+    const strStrip = str.trim();
+    if (strStrip.length > 0) {
+      const newDiv = ChatView.PostSelf(this.refs.chatinput.value);
+      this.addMessages(newDiv);
+      this.refs.chatinput.value = '';
+    } else {
+      this.refs.chatinput.value = str.slice(0, str.length - 1);
+    }
+  }
+
+  catchSubmit(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+      e.preventDefault();
+      this.handleSubmit();
+    }
+    return true;
+  }
+
+  render() {
+    return (
+      <div className="container" id="chat-body">
+        <div className="container-fluid" id="chat-content-container">
+          {this.state.messages}
+        </div>
+        <div className="fixed-bottom" id="chat-form-container">
+          <textarea
+            className="form-control"
+            ref="chatinput"
+            id="chat-input"
+            placeholder={this.placeholder}
+            onKeyDown={this.catchSubmit.bind(this)}
+          />
+          <button
+            type="button"
+            id="chat-submit-button"
+            className="btn btn-default"
+            onClick={this.handleSubmit.bind(this)}
+          >
+            <img
+              id="chat-submit-image"
+              src="../../resources/images/ic_send_24dp.png"
+              alt="Submit"
+            />
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
