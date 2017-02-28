08052cd0bdbae350bff72fd992d6d5c251a435df
Muhammad-Adam <splitfire_1992@hotmail.com>
2017-02-28 15:42:17 +0800

    Fixed navbar template with proper links
    Havent yet be able to check is user signed in/out

--- app/components/app.js
+++ app/components/app.js
@@ -6,20 +6,29 @@ import Paths from "../paths"
 class App extends React.Component {
   render() {
     return(
-      <div className="container-fluid">
-        <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top row justify-content-between collapse navbar-collapse">
-          <Link className="col-4" to={Paths.home}><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
-          <Link to={Paths.profile}>Profile</Link>
-          <Link to={Paths.event}>Event</Link>
-          <Link to={Paths.chat}>Chat</Link>
-          <div id="user-authentication-buttons" className="col-8 col-md-4 text-right">
-            { Auth.isUserAuthenticated() ?
-              <Link to={Paths.logout}><button id="logout" type="button" className="btn btn-secondary">Logout</button></Link> :
-              <div>
-                <Link to={Paths.login}><button id="login" type="button" className="btn-sm btn-primary">Login</button></Link>
-                <Link to={Paths.signup}><button id="signup" type="button" className="btn-sm btn-secondary">Sign Up</button></Link>
-              </div>
-            }
+      <div>
+        <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link to={Paths.home}><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to={Paths.profile}>Profile</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={Paths.event}>Event</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={Paths.chat}>Chat</Link>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
           </div>
         </nav>
         <div id="app-body">
@@ -35,3 +44,15 @@ App.propTypes = {
 };
 
 export default App;
+
+/*
+    <div id="user-authentication-buttons" className="col-8 col-md-4 text-right">
+      { Auth.isUserAuthenticated() ?
+        <Link to={Paths.logout}><button id="logout" type="button" className="btn btn-secondary">Logout</button></Link> :
+        <div>
+          <Link to={Paths.login}><button id="login" type="button" className="btn-sm btn-primary">Login</button></Link>
+          <Link to={Paths.signup}><button id="signup" type="button" className="btn-sm btn-secondary">Sign Up</button></Link>
+        </div>
+      }
+    </div>
+  */
