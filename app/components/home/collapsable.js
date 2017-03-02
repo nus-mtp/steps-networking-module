dc7a14686b051c2af0c2b9d936577d0a6c253bac
Eric Ewe <eric.ewe@zalora.com>
2017-03-02 17:17:55 +0800

    Restructure collapsable layout

--- app/components/home/collapsable.js
+++ app/components/home/collapsable.js
@@ -1,15 +1,51 @@
 import React from 'react';
 
 class Collapsable extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      numberOfEventPerRow: Math.floor((window.innerWidth - 35) / this.props.width),
+      order: this.props.serial,
+    };
+  
+    this.setLayout = this.setLayout.bind(this);
+    this.updateLayout = this.updateLayout.bind(this);
+  }
+  
+  componentDidMount() {
+    window.addEventListener('resize', this.updateLayout, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateLayout);
+  }
+
+  updateLayout() {
+    this.setState({
+      numberOfEventPerRow: Math.floor((window.innerWidth - 35) / this.props.width),
+    });
+  }
+  
+  setLayout() {
+    let marginToDisplay = Math.abs(this.state.order % this.state.numberOfEventPerRow);
+    
+    return {
+      width: `${this.state.numberOfEventPerRow}00%`,
+      borderTop: 'none',
+      marginLeft: `-${marginToDisplay}00%`,
+      marginTop: '-1px',
+    };
+  }
+  
   render() {
     let showCollapse = (this.props.open[this.props.serial]) ? 'collapse show' : 'collapse';
+    
     return (
-      <div className={showCollapse} id={"collapseExample"+this.props.serial-1} aria-expanded={this.props.open[this.props.serial]}>
-        <div className="card card-block">
+      <div style={this.setLayout()} className={showCollapse}>
+        <div className="collapse-body card card-block">
           <div>
-            <h6>Title {this.props.serial}</h6>
-            <div id="event-title" />
-            <h6>Description {this.props.serial}</h6>
+            <div id="event-title"><h6>{this.props.file.name}</h6></div>
             <div id="event-description" />
           </div>
         </div>
@@ -17,5 +53,14 @@ class Collapsable extends React.Component {
     );
   }
 }
+
+
+Collapsable.propTypes = {
+  serial: React.PropTypes.number.isRequired,
+  open: React.PropTypes.array.isRequired,
+  openCollapsable: React.PropTypes.func.isRequired,
+  width: React.PropTypes.number.isRequired,
+  file: React.PropTypes.object,
+};
 
 export default Collapsable;
