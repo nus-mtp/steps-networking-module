71aa5a7cd7890e732740eb3b12bc61b021dd70b2
Jean <jkohx3@gmail.com>
2017-03-02 10:53:25 +0800

    Added static functions to clear attendance, get all attendance, get attendance object and connect/disconnect DB

--- app/database/objectClasses/Attendance.js
+++ app/database/objectClasses/Attendance.js
@@ -4,21 +4,59 @@ const host = "localhost";
 const dbName = "fake-data";
 
 class Attendance{
-    //ASSUMPTION: usermail and eventID are checked and existing.
-    constructor(userEmail, attendanceName, attendanceType,reason) {
-      this.ModelHandler = new ModelHandler(host, port, db);
-      this.attendanceModel = this.ModelHandler.getAttendanceModel();
-      this.attendanceModelDoc = new this.attendanceModel({
-        user_email:UserEmail,
-        attendance_name: attendance_name,
-        attendance_type: attendanceType,
-        reason: reason,
-      });
-     this.attendanceModelDoc.save(function (err){
-       if (err){
-         console.log("Unable to save new attendance");
-       }
-     });   
-    }
+  //ASSUMPTION: usermail and eventID are checked and existing.
+  constructor(userEmail, attendanceName, attendanceType,reason) {
+    this.ModelHandler = new ModelHandler(host, port, dbName);
+    this.attendanceModel = this.ModelHandler.getAttendanceModel();
+    this.attendanceModelDoc = new this.attendanceModel({
+      user_email:userEmail,
+      attendance_name: attendanceName,
+      attendance_type: attendanceType,
+      reason: reason,
+    });
+    this.attendanceModelDoc.save(function (err){
+      if (err){
+        console.log("Unable to save new attendance");
+        console.log(err);
+      }
+    });   
+    this.ModelHandler.disconnect();
+  }
+
+  static connectDB(){
+    this.ModelHandler = new ModelHandler (host, port, dbName);
+    this.attendanceModel = this.ModelHandler.getAttendanceModel();
+  }
+
+  static disconnectDB(){
+    this.ModelHandler.disconnect();
+  }
+
+  static getAllAttendance(callback){
+    Attendance.connectDB();
+    this.attendanceModel.find({},function (err,userDoc){
+      if (err){
+        callback(err,null);
+      } else{
+        callback(null,userDoc);
+      }
+    });
+    Attendance.disconnectDB();
+  }
+
+  /**
+   * Remove the collection of Users from database
+   */
+  static clearAllAttendance(){
+    Attendance.connectDB();
+    this.attendanceModel.collection.remove({});
+    Attendance.disconnectDB();
+  }
+
+  static getAttendanceObj(callback){
+    Attendance.connectDB();
+    this.attendanceModel.find({});
+    Attendance.disconnectDB();
+  }
 }
 module.exports = Attendance;
