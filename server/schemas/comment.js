c3ac88655676088b6d2e6faea578263a2d566503
Sean <sean@u.nus.edu>
2017-03-07 17:46:20 +0800

    Linted Schema definitions.

--- server/schemas/comment.js
+++ server/schemas/comment.js
@@ -1,23 +1,24 @@
-var mongoose = require('mongoose');
-var db = require('../mongodbScripts/accessMongoDB');
+const mongoose = require('mongoose');
 
-var commentSchema = new mongoose.Schema({
-    user_email: {
-        type: String,
-        required: "The Email of the User who posted this Comment is used as a Foreign Key, and is therefore Required.",
-    },
-    exhibition: {
-        type: String,
-        required: "The Name of the Exhibition for which this Comment is posted under is used as a Foreign Key, and is therefore Required.",
-    },
-    comment: {
-        type: String,
-        trim: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+const commentSchema = new mongoose.Schema({
+  user_email: {
+    type: String,
+    required: 'The Email of the User who posted this Comment is used as a Foreign Key, and is t' +
+        'herefore Required.',
+  },
+  exhibition: {
+    type: String,
+    required: 'The Name of the Exhibition for which this Comment is posted under is used as a F' +
+        'oreign Key, and is therefore Required.',
+  },
+  comment: {
+    type: String,
+    trim: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = commentSchema;
--- server/schemas/event.js
+++ server/schemas/event.js
@@ -1,27 +1,29 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var eventSchema = new mongoose.Schema({
-    event_name: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: "The Event Name is a Primary Key, and is therefore Required. ",
-        index: true,
-    },
-    event_description: String,
-    start_date: {
-        type: Date,
-        default: Date.now,
-    },
-    end_date: {
-        type: Date,
-        default: function() { return Date.now() + 24 * 60 * 60 * 1000 },
-    },
-    event_location: String,
-    event_map: String,
-    event_picture: String,
+const eventSchema = new mongoose.Schema({
+  event_name: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: 'The Event Name is a Primary Key, and is therefore Required.',
+    index: true,
+  },
+  event_description: String,
+  start_date: {
+    type: Date,
+    default: Date.now,
+  },
+  end_date: {
+    type: Date,
+    default: () => {
+      return Date.now() + (24 * 60 * 60 * 1000);
+    }
+  },
+  event_location: String,
+  event_map: String,
+  event_picture: String,
 
-    tags: [String]
+  tags: [String],
 });
 
 module.exports = eventSchema;
