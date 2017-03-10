d579079e426cbcfe7e5ed8fa36ab58b9111663ba
Eric Ewe <eric.ewe@zalora.com>
2017-03-11 04:42:49 +0800

    Create sample data for test

---
+++ app/components/project/sampleData.js
@@ -0,0 +1,25 @@
+const SampleProjects = [
+  {
+    exhibitionName: "Monalisa",
+    eventName: "Test",
+    tags: [
+      "intern", "co-founder",
+    ],
+  },
+  {
+    exhibitionName: "Alan Turing's Apple",
+    eventName: "Test",
+    tags: [
+      "intern", "full-time",
+    ],
+  },
+  {
+    exhibitionName: "Donald Trump's Hair",
+    eventName: "Test",
+    tags: [
+      "full-time",
+    ],
+  },
+]
+
+export default SampleProjects;
