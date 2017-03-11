a02255cb645e79af7bb8e4f3bf47dd50a127c648
Sean <sean@u.nus.edu>
2017-03-11 13:50:53 +0800

    Added automatic trimming of more attributes in attendanceSchema.

--- server/database/schemas/ourSchemas/attendance.js
+++ server/database/schemas/ourSchemas/attendance.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const attendanceSchema = new mongoose.Schema({
   user_email: {
     type: String,
+    trim: true,    
     required: 'The Email of the User who is attending the Event / Exhibition is used as a Foreign Key, and is t' +
         'herefore Required.',
   },
@@ -15,6 +16,7 @@ const attendanceSchema = new mongoose.Schema({
   },
   attendance_name: {
     type: String, // Event or Exhibition Name
+    trim: true,
     required: 'The Name of the Event or Exhibition for the User is attending is us' +
         'ed as a Foreign Key, and is therefore Required.',
   },
