b5f66233dad4dc129fe4a41a8c294107794d53d4
Eric Ewe <eric.ewe@zalora.com>
2017-03-13 11:19:23 +0800

    Remove redundant code and stylings

--- app/components/app.js
+++ app/components/app.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router';
 import Auth from '../database/auth';
 import Paths from '../paths';
-import Bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  onClick() {
+    this.removeDropdown = this.removeDropdown;
+  }
+
+  removeDropdown() {
     document.getElementById("navbarSupportedContent").classList.remove("show");
   }
 
@@ -14,41 +18,38 @@ class App extends React.Component {
     return(
       <div>
         <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-          <button id="abc" className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button id="hamburger-toggle" className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <Link  onClick={this.onClick.bind(this)} to={Paths.home}><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
-
+          <Link onClick={this.removeDropdown} to={Paths.home}><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-              <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            <form className="form-inline mt-2 mt-lg-0 row">
+              <input id="search-input" className="form-control col-9" type="text" placeholder="Search" />
+              <button id="search-submit" className="btn btn-secondary col-3" type="submit">Search</button>
             </form>
-            <ul className="navbar-nav">
+            <ul id="navbar-links" className="navbar-nav">
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.onClick.bind(this)} className="link nav-link" to={Paths.profile}>Profile</Link> :
+                  <Link onClick={this.removeDropdown.bind(this)} className="link nav-link" to={Paths.profile}>Profile</Link> :
                   <Link></Link> }
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.onClick.bind(this)} className="link nav-link" to={Paths.event}>Event</Link> :
+                  <Link onClick={this.removeDropdown.bind(this)} className="link nav-link" to={Paths.event}>Event</Link> :
                   <Link></Link> }
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.onClick.bind(this)} className="link nav-link" to={Paths.chat}>Chat</Link> :
+                  <Link onClick={this.removeDropdown.bind(this)} className="link nav-link" to={Paths.chat}>Chat</Link> :
                   <Link></Link> }
-
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.onClick.bind(this)} id="logout" className="link nav-link" to={Paths.logout}>Logout</Link> :
-                  <Link onClick={this.onClick.bind(this)} id="login" className="link nav-link" to={Paths.login}>Login</Link> }
+                  <Link onClick={this.removeDropdown.bind(this)} id="logout" className="link nav-link" to={Paths.logout}>Logout</Link> :
+                  <Link onClick={this.removeDropdown.bind(this)} id="login" className="link nav-link" to={Paths.login}>Login</Link> }
               </li>
             </ul>
           </div>
-
         </nav>
         <div id="app-body">
           {this.props.children}
@@ -63,15 +64,3 @@ App.propTypes = {
 };
 
 export default App;
-
-/*
-    <div id="user-authentication-buttons" className="col-8 col-md-4 text-right">
-      { Auth.isUserAuthenticated() ?
-        <Link to={Paths.logout}><button id="logout" type="button" className="btn btn-secondary">Logout</button></Link> :
-        <div>
-          <Link to={Paths.login}><button id="login" type="button" className="btn-sm btn-primary">Login</button></Link>
-          <Link to={Paths.signup}><button id="signup" type="button" className="btn-sm btn-secondary">Sign Up</button></Link>
-        </div>
-      }
-    </div>
-  */
