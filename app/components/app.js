45a65dbd238a84a60d885bb753ffa05f2976bad7
Eric Ewe <eric.ewe@zalora.com>
2017-03-13 13:52:09 +0800

    Fix merge bugs

--- app/components/app.js
+++ app/components/app.js
@@ -11,9 +11,11 @@ class App extends React.Component {
       loginActive: '',
       eventActive: '',
       chatActive: '',
+      isHamburgerToggled: false,
     };
-    this.removeDropdown = this.removeDropdown;
+    this.removeDropdown = this.removeDropdown.bind(this);
     this.handleLinks = this.handleLinks.bind(this);
+    this.shiftBody = this.shiftBody.bind(this);
   }
 
   componentWillReceiveProps() {
@@ -21,7 +23,25 @@ class App extends React.Component {
   }
 
   removeDropdown() {
-    document.getElementById("navbarSupportedContent").classList.remove("show");
+    document.getElementById("navbar-supported-content").classList.remove("show");
+    document.getElementById("app-body").classList.add("collapse-hide");
+    document.getElementById("app-body").classList.remove("collapse-show");
+    this.setState({
+      isHamburgerToggled: !this.state.isHamburgerToggled,
+    });
+  }
+
+  shiftBody() {
+    if (!this.state.isHamburgerToggled) {
+      document.getElementById("app-body").classList.remove("collapse-hide");
+      document.getElementById("app-body").classList.add("collapse-show");
+    } else {
+      document.getElementById("app-body").classList.add("collapse-hide");
+      document.getElementById("app-body").classList.remove("collapse-show");
+    }
+    this.setState({
+      isHamburgerToggled: !this.state.isHamburgerToggled,
+    });
   }
 
   handleLinks() {
@@ -48,41 +68,45 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <nav id="header" className="navbar navbar-toggleable-md navbar-light fixed-top row justify-content-between navbar-collapse">
-          <button id="hamburger-toggle" className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav id="header" className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+          <button id="hamburger-toggle" onClick={this.shiftBody} className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-supported-content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <Link onClick={this.removeDropdown} to={Paths.home}><img id="brand-logo" src="resources/images/home.svg" alt="Home"/></Link>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <form className="form-inline mt-2 mt-lg-0 row">
+          <div className="collapse navbar-collapse flex-column flex-lg-row justify-content-between" id="navbar-supported-content">
+            <form className="form-inline mt-2 mt-lg-0 row" id="search-container">
               <input id="search-input" className="form-control col-9" type="text" placeholder="Search" />
               <button id="search-submit" className="btn btn-secondary col-3" type="submit">Search</button>
             </form>
             <ul id="navbar-links" className="navbar-nav">
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.profileActive} to={Paths.profile}>Profile</Link> :
-                  <Link></Link> }
+                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.profileActive}`} to={Paths.profile}>Profile</Link> :
+                  <Link></Link>
+                }
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.eventActive} to={Paths.event}>Event</Link> :
-                  <Link></Link> }
+                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.eventActive}`} to={Paths.event}>Event</Link> :
+                  <Link></Link>
+                }
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.chatActive} to={Paths.chat}>Chat</Link> :
-                  <Link></Link> }
+                  <Link onClick={this.removeDropdown.bind(this)} className={`navbar-buttons ${this.state.chatActive}`} to={Paths.chat}>Chat</Link> :
+                  <Link></Link>
+                }
               </li>
               <li className="nav-item">
                 { Auth.isUserAuthenticated() ?
-                  <Link onClick={this.removeDropdown.bind(this)} id="logout" className={`navbar-buttons ${this.state.logoutActive} to={Paths.logout}>Logout</Link> :
-                  <Link onClick={this.removeDropdown.bind(this)} id="login" className={`navbar-buttons ${this.state.loginActive} to={Paths.login}>Login</Link> }
+                  <Link onClick={this.removeDropdown.bind(this)} id="logout" className="navbar-buttons" to={Paths.logout}>Logout</Link> :
+                  <Link onClick={this.removeDropdown.bind(this)} id="login" className={`navbar-buttons ${this.state.loginActive}`} to={Paths.login}>Login</Link>
+                }
               </li>
             </ul>
           </div>
         </nav>
-        <div id="app-body">
+        <div id="app-body" className="collapse-hide">
           {this.props.children}
         </div>
       </div>
