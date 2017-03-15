68727d206733f0b0adf07fae8aac10f8509e2065
Jean <jkohx3@gmail.com>
2017-03-16 01:23:32 +0800

    Object class for Comments created with Create and Read Functions. Delete is not appliable. Update is subjective to Changes. Unit test is created for Create and Read functions.

---
+++ app/database/objectClasses/Comment.js
@@ -0,0 +1,99 @@
+const ModelHandler = require('../../../server/database/models/ourModels.js');
+
+const port = '27017';
+const host = 'localhost';
+const dbName = 'dev';
+const username = '';
+const password = '';
+
+/**
+ * This is the wrapper class used extract out and store information
+ * about the comments from the database between view and model
+ *
+ */
+class Comment{
+
+  constructor(userEmail, exhibitionName, comment, date) {
+    this.ModelHandler = new ModelHandler().initWithParameters(username, password, host, port, dbName);
+    this.CommentModel = this.ModelHandler.getCommentModel();
+    this.commentModelDoc = new this.CommentModel({
+      user_email: userEmail,
+      exhibition: exhibitionName,
+      comment: comment,
+      date: date,
+    });
+    this.ModelHandler.disconnect();
+  }
+
+
+  /**
+   * saves comment into Database
+   *
+   * @param {function} callback: used for error checking
+   */
+  saveComment(callback) {
+    Comment.connectDB();
+    this.commentModelDoc.save(function cb(err) {
+      callback(err);
+    });
+    Comment.disconnectDB();
+  }
+
+  static connectDB() {
+    this.ModelHandler = new ModelHandler().initWithParameters(username, password, host, port, dbName);
+    this.CommentModel = this.ModelHandler.getCommentModel();
+  }
+
+  static disconnectDB() {
+    this.ModelHandler.disconnect();
+  }
+
+  /**
+   * Removes all event from the Database
+   *
+   * @param {function} callback: used for error checking
+   */
+  static clearAllComment(callback) {
+    Comment.connectDB();
+    this.CommentModel.collection.remove({}, callback);
+    Comment.disconnectDB();
+  }
+
+  static clearCommentsForExhibition(exhibitionName, callback){
+    Comment.connectDB();
+    this.CommentModel.findOneAndRemove({ exhibition: exhibitionName }, callback);
+    Comment.disconnectDB();
+  }
+
+  static getCommentForExhibition(exhibitionName, callback) {
+    Comment.connectDB();
+    this.CommentModel.find({ exhibition: exhibitionName }, function cb(err, commentArr){
+      if (err){
+        callback(err, null);
+      } else {
+        callback(null, commentArr);
+      }
+    });
+    Comment.disconnectDB();
+  }
+
+  static addCommentForExhibition(userEmail, exhibitionName, comment, date, callback){ //does this mean can onli have one unique object with userEmail and exhibition?
+    Comment.connectDB();
+    const query = {
+      user_email: userEmail,
+      exhibition_name: exhibitionName,
+    };
+    const update = {$push: { content: content, date: date } };
+    const options = { new: true };
+    this.CommentModel.findOne(query, update, options, function cb(err, results) {
+      if (err) {
+        callback(err, null);
+      } else {if (results) {
+        callback(null, results);
+      } else {
+        callback(null, null);
+      }}
+    });
+    Comment.disconnectDB();
+  }
+} module.exports = Comment;
