076f904d474d478cd8c20a4bd7eaddeca445f0a1
Sean <sean@u.nus.edu>
2017-03-16 17:56:41 +0800

    Incorporated the User objectClass into the route handler.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,27 +1,21 @@
 const express = require('express');
+
 const router = new express.Router();
+
+const User = require('../database/objectClasses/User');
 
 import currentdb from '../currentdb';
 const config = require('../config.json');
 const ModelHandler = require('../database/models/ourModels');
 
-router.get('/get/:name', (req = {}, res, next) => {
-  const ModelHandlerObject = new ModelHandler().initWithParameters(config[currentdb].username,
-                                                      config[currentdb].password,
-                                                      config[currentdb].host,
-                                                      config[currentdb].port,
-                                                      config[currentdb].database);
-
-  const User = ModelHandlerObject.getUserModel();
-
-  User.where({ name: req.params.name }).lean().findOne((err, user) => {
+router.get('/get/:email', (req = {}, res, next) => {
+  User.getUser(req.params.email, (err, user) => {
     if (err) {
+      console.log(err);
       res.status(500).json('Unable to fetch data!');
     } else {
       res.status(200).json(user);
     }
-
-    ModelHandlerObject.disconnect();
 
     next();
   });
