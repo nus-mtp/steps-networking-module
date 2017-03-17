d76e49e631b42d2b0ad954b58114563a497a1a1a
Sean <sean@u.nus.edu>
2017-03-17 17:49:19 +0800

    Fixed typo in name.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -16,15 +16,15 @@ router.get('/get/allExhibitions', (req = {}, res, next) => {
       res.status(500).json('Unable to fetch data!');
       next();
     } else if (exhibitionObjs) {
-      res.status(200).json(exhibitionObjs.map(exhibitonObj => ({
-        exhibitionName: exhibitonObj.exhibition_name,
-        exhibitionDescription: exhibitonObj.exhibition_description,
-        eventName: exhibitonObj.event_name,
-        website: exhibitonObj.website,
-        poster: exhibitonObj.poster,
-        images: exhibitonObj.images,
-        videos: exhibitonObj.videos,
-        tags: exhibitonObj.tags,
+      res.status(200).json(exhibitionObjs.map(exhibitionObj => ({
+        exhibitionName: exhibitionObj.exhibition_name,
+        exhibitionDescription: exhibitionObj.exhibition_description,
+        eventName: exhibitionObj.event_name,
+        website: exhibitionObj.website,
+        poster: exhibitionObj.poster,
+        images: exhibitionObj.images,
+        videos: exhibitionObj.videos,
+        tags: exhibitionObj.tags,
       })));
       next();
     } else {
