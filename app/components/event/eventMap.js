acdc76b11988aa3ae4ea9a391cf91b19af8025b6
Vylantze <digi9346@hotmail.com>
2017-03-17 17:41:10 +0800

    Fixed buttons not adhering to appropriate positions and added Alan Turing's Apple button

--- app/components/event/eventMap.js
+++ app/components/event/eventMap.js
@@ -7,10 +7,10 @@ class EventMap extends React.Component {
   }
   
   // Adds a clickable button to the map that jumps to a project with 'id'
-  static addNode(id = '#0', name = 'None', left = '0%', top = '0px') { 
+  static addNode(id = '#0', name = (<div />), left = '0%', top = '0px', divStyle = {}) {
     return (
-      <a href={id} style={{ position: 'relative', left, top, }}>
-        <button className="btn btn-secondary">
+      <a href={id} style={{position: 'absolute', left, top,}}>
+        <button className="btn btn-secondary" id="event-map-pointers" style={divStyle}>
           {name}
         </button>
       </a>
@@ -22,8 +22,10 @@ class EventMap extends React.Component {
       <div>
       {
         (this.props.showEventMap) ?
-        <div className="mb-3" id="event-map">
-          {EventMap.addNode('#0', 'MonaLisa', '78%', '99px')}
+        <div className="mb-3" id="event-map" style={{position: 'relative'}}>
+          {EventMap.addNode('#0', 'MonaLisa', '79%', '29%')} 
+          {EventMap.addNode('#1', <div>Alan<br/>Turing's<br/>Apple</div>, '13%', '68%',
+                           { backgroundColor: 'blue', color: 'white', })}
         </div>
         : <div />
       }
