0cb38cb56321715fda0f8d1c090c52cc653c4d8d
Sean <sean@u.nus.edu>
2017-03-17 17:48:28 +0800

    Adjusted indentation for exhibiton route file.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -16,18 +16,16 @@ router.get('/get/allExhibitions', (req = {}, res, next) => {
       res.status(500).json('Unable to fetch data!');
       next();
     } else if (exhibitionObjs) {
-      res.status(200).json(exhibitionObjs.map((exhibitonObj) => {
-          return {
-              exhibitionName: exhibitonObj.exhibition_name,
-              exhibitionDescription: exhibitonObj.exhibition_description,
-              eventName: exhibitonObj.event_name,
-              website: exhibitonObj.website,
-              poster: exhibitonObj.poster,
-              images: exhibitonObj.images,
-              videos: exhibitonObj.videos,
-              tags: exhibitonObj.tags,
-          };
-      }));
+      res.status(200).json(exhibitionObjs.map(exhibitonObj => ({
+        exhibitionName: exhibitonObj.exhibition_name,
+        exhibitionDescription: exhibitonObj.exhibition_description,
+        eventName: exhibitonObj.event_name,
+        website: exhibitonObj.website,
+        poster: exhibitonObj.poster,
+        images: exhibitonObj.images,
+        videos: exhibitonObj.videos,
+        tags: exhibitonObj.tags,
+      })));
       next();
     } else {
       res.status(404).json('Nothing found!');
