e47c43aa78de0ddcdd20c07b2486644ca3207608
Vylantze <digi9346@hotmail.com>
2017-03-17 15:57:42 +0800

    Abstracted the function that makes the button for EventMap

--- app/components/event/eventMap.js
+++ app/components/event/eventMap.js
@@ -6,17 +6,24 @@ class EventMap extends React.Component {
     
   }
   
+  // Adds a clickable button to the map that jumps to a project with 'id'
+  static addNode(id = '#0', name = 'None', left = '0%', top = '0px') { 
+    return (
+      <a href={id} style={{ position: 'relative', left, top, }}>
+        <button className="btn btn-secondary">
+          {name}
+        </button>
+      </a>
+    );
+  }
+  
   render() {
     return (
       <div>
       {
         (this.props.showEventMap) ?
         <div className="mb-3" id="event-map">
-          <a href="#0" style={{ position: 'relative', left: '78%', top: '99px', }}>
-            <button className="btn btn-secondary">
-              MonaLisa
-            </button>
-          </a>
+          {EventMap.addNode('#0', 'MonaLisa', '78%', '99px')}
         </div>
         : <div />
       }
