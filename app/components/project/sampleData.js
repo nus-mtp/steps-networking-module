5be7f6a48026ae7d3ff3764a7dc796cbe1cf0b9b
Eric Ewe <eric.ewe@zalora.com>
2017-03-18 14:53:51 +0800

    Refactor sample data

--- app/components/project/sampleData.js
+++ app/components/project/sampleData.js
@@ -1,4 +1,4 @@
-const SampleProjects = [
+const sampleProjects = [
   {
     exhibitionName: "Monalisa",
     eventName: "New Year Wild Party",
@@ -20,6 +20,21 @@ const SampleProjects = [
       "full-time",
     ],
   },
-]
+];
 
-export default SampleProjects;
+const sampleComments = [
+  {
+    email: "troll@troll.com",
+    comments : [
+      "hello", "bye bye", "you suck", "lame project",
+    ]
+  },
+  {
+    email: "kfc@kfc.com",
+    comments : [
+      "eat", "hungry",
+    ]
+  },
+];
+
+export { sampleProjects, sampleComments };
