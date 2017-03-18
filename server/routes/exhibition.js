ca90ae8ed3a43da5742108d38c1a3510974b2503
Sean <sean@u.nus.edu>
2017-03-18 23:16:44 +0800

    Added a route for getting all exhibitions under an Event.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -6,6 +6,31 @@ const Exhibition = require('../database/objectClasses/Exhibition');
 
 // All Routes prefixed with 'exhibition/'
 
+router.get('/get/oneEventExhibition/:eventName', (req = {}, res, next) => {
+  Exhibition.searchExhibitionsByEvent(req.params.eventName, (err, exhibitionObjs) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('Unable to fetch data!');
+      next();
+    } else if (exhibitionObjs) {
+      res.status(200).json(exhibitionObjs.map(exhibitionObj => ({
+        exhibitionName: exhibitionObj.exhibition_name,
+        exhibitionDescription: exhibitionObj.exhibition_description,
+        eventName: exhibitionObj.event_name,
+        website: exhibitionObj.website,
+        poster: exhibitionObj.poster,
+        images: exhibitionObj.images,
+        videos: exhibitionObj.videos,
+        tags: exhibitionObj.tags,
+      })));
+      next();
+    } else {
+      res.status(404).json('Nothing found!');
+      next();
+    }
+  });
+});
+
 router.get('/get/allExhibitions', (req = {}, res, next) => {
   Exhibition.getAllExhibitions((err, exhibitionObjs) => {
     if (err) {
@@ -31,8 +56,9 @@ router.get('/get/allExhibitions', (req = {}, res, next) => {
   });
 });
 
-router.get('/get/oneEventExhibition/:eventName', (req = {}, res, next) => {
-  Exhibition.searchExhibitionsByEvent(req.params.eventName, (err, exhibitionObjs) => {
+/*
+router.get('/get/:exhibitionName', (req = {}, res, next) => {
+  Exhibition.getExhibition(req.param.exhibitionName, (err, exhibitionObjs) => {
     if (err) {
       console.log(err);
       res.status(500).json('Unable to fetch data!');
@@ -48,12 +74,12 @@ router.get('/get/oneEventExhibition/:eventName', (req = {}, res, next) => {
         videos: exhibitionObj.videos,
         tags: exhibitionObj.tags,
       })));
-      next();
     } else {
       res.status(404).json('Nothing found!');
       next();
     }
   });
 });
+*/
 
 module.exports = router;
