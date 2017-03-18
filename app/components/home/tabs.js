32d15c307cf20e704abc77cbf4350746660acffc
Muhammad-Adam <splitfire_1992@hotmail.com>
2017-03-18 19:06:45 +0800

    Tabs now become active when clicked

--- app/components/home/tabs.js
+++ app/components/home/tabs.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import Tab from './tab';
 
 class Tabs extends React.Component {
   render() {
     return (
       <div className="event-tabs">
-        <ul className="nav nav-tabs justify-content-around">
-          <Tab name="Ongoing" />
-          <Tab name="Upcoming" />
-          <Tab name="Past" />
+        <ul className="nav nav-tabs justify-content-between" role="tablist">
+          <li className="nav-item">
+            <a className="nav-link active" data-toggle="tab" href="#ongoing" role="tab">Ongoing</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" data-toggle="tab" href="#upcoming" role="tab">Upcoming</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" data-toggle="tab" href="#past" role="tab">Past</a>
+          </li>
         </ul>
+
+        <div className="tab-content">
+          <div className="tab-pane active" id="ongoing" role="tabpanel"></div>
+          <div className="tab-pane" id="upcoming" role="tabpanel"></div>
+          <div className="tab-pane" id="past" role="tabpanel"></div>
+        </div>
       </div>
     );
   }
