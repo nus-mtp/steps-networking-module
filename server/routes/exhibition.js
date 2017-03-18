937419e6cfe0350d772396671fd802bc02445262
Sean <sean@u.nus.edu>
2017-03-18 12:19:46 +0800

    Added route to get all exhibitions from a single Event.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -34,4 +34,29 @@ router.get('/get/allExhibitions', (req = {}, res, next) => {
   });
 });
 
+router.get('/get/oneEventExhibition/:eventName', (req = {}, res, next) => {
+  Exhibition.searchExhibitionsByEvent(req.params.eventName, (err, exhibitionObjs) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('Unable to fetch data!');
+      next();
+    } else if (exhibitionObjs) {
+      res.status(200).json(exhibitionObjs.map(exhibitionObj => ({
+        exhibitionName: exhibitionObj.exhibition_name,
+        exhibitionDescription: exhibitionObj.exhibition_description,
+        eventName: exhibitionObj.event_name,
+        website: exhibitionObj.website,
+        poster: exhibitionObj.poster,
+        images: exhibitionObj.images,
+        videos: exhibitionObj.videos,
+        tags: exhibitionObj.tags,
+      })));
+      next();
+    } else {
+      res.status(404).json('Nothing found!');
+      next();
+    }
+  });
+});
+
 module.exports = router;
