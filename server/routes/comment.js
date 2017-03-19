0bcb752b66da02443223bdab5ab11e94e64df2e9
Sean <sean@u.nus.edu>
2017-03-19 23:06:10 +0800

    Linted comment and message routes. Added next() invocation. Changed some messages from 'fetch' to 'save'.

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,29 +1,37 @@
 const express = require('express');
+
 const router = new express.Router();
-const Comment = require ('../database/objectClasses/Comment.js');
 
+const Comment = require('../database/objectClasses/Comment.js');
 
-router.post('/post/newComment',(req = {}, res, next) => {
-  const newComment = new Comment (req.body.userEmail, req.body.exhibitionName, req.body.comment, Date.now());
-  newComment.saveComment((err)=>{
+router.post('/post/newComment', (req = {}, res, next) => {
+  const newComment = new Comment(
+      req.body.userEmail, req.body.exhibitionName, req.body.comment, Date.now());
+  newComment.saveComment((err) => {
     if (err) {
-      res.status(500).json('Unable to fetch data!');
+      res.status(500).json('Unable to save data!');
+      next();
     } else {
       res.status(200).send('Added');
+      next();
     }
   });
 });
 
 router.post('/post/addComment', (req = {}, res, next) => {
-  Comment.addCommentForExhibition(req.body.userEmail, req.body.exhibitionName, req.body.comment, Date.now(), (err, results) => {
-    if (err) {
-      res.status(500).json('Unable to fetch data!');
-    } else if (results){
-      res.status(200).send('Added');
-    } else {
-      res.status(404).json('Comment object not found!');
-    }
-  });
+  Comment.addCommentForExhibition(
+      req.body.userEmail, req.body.exhibitionName, req.body.comment, Date.now(), (err, results) => {
+        if (err) {
+          res.status(500).json('Unable to fetch data!');
+          next();
+        } else if (results) {
+          res.status(200).send('Added');
+          next();
+        } else {
+          res.status(404).json('Comment object not found!');
+          next();
+        }
+      });
 });
 
 module.exports = router;
--- server/routes/message.js
+++ server/routes/message.js
@@ -1,31 +1,37 @@
 const express = require('express');
+
 const router = new express.Router();
-const Message = require ('../database/objectClasses/Message.js');
+
+const Message = require('../database/objectClasses/Message.js');
 
 router.get('/get/getMessages/:senderEmail/:recipientEmail', (req = {}, res, next) => {
-
-  Message.getConversation(req.params.senderEmail, req.params.recipientEmail, function (err, msgObjs){
+  Message.getConversation(req.params.senderEmail, req.params.recipientEmail, (err, msgObjs) => {
     if (err) {
       res.status(500).json('Unable to fetch data!');
+      next();
     } else if (msgObjs) {
       res.status(200).json({
         sender: msgObjs.sender_email,
         receiver: msgObjs.recipient_email,
         messages: msgObjs.messages,
       });
+      next();
     } else {
       res.status(404).json('Not found!');
     }
+    next();
   });
 });
 
-router.post('/post/newMessage',(req = {}, res, next) => {
-  const newMessage = new Message (req.body.senderEmail, req.body.recipientEmail, req.body.content, Date.now());
-  newMessage.saveMessage((err)=>{
+router.post('/post/newMessage', (req = {}, res, next) => {
+  const newMessage = new Message(req.body.senderEmail, req.body.recipientEmail, req.body.content, Date.now());
+  newMessage.saveMessage((err) => {
     if (err) {
-      res.status(500).json('Unable to fetch data!');
+      res.status(500).json('Unable to save data!');
+      next();
     } else {
-      res.status(200).send('Added');
+      res.status(200).send('Added!');
+      next();
     }
   });
 });
@@ -33,11 +39,14 @@ router.post('/post/newMessage',(req = {}, res, next) => {
 router.post('/post/addMessage', (req = {}, res, next) => {
   Message.addMessage(req.body.senderEmail, req.body.recipientEmail, req.body.content, Date.now(), (err, results) => {
     if (err) {
-      res.status(500).json('Unable to fetch data!');
-    } else if (results){
-      res.status(200).send('Added');
+      res.status(500).json('Unable to save data!');
+      next();
+    } else if (results) {
+      res.status(200).send('Added!');
+      next();
     } else {
       res.status(404).json('Message object not found!');
+      next();
     }
   });
 });
