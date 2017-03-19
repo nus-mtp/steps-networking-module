b8f9d67a99e5a29cb71a5eaac78e6a9bbfc925fe
Sean <sean@u.nus.edu>
2017-03-19 23:28:42 +0800

    Added route for specifying the ability to get just one Exhibition.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -58,5 +58,32 @@ router.get('/get/allExhibitions', (req = {}, res, next) => {
   });
 });
 
+router.get('/get/oneExhibition/:eventName/:exhibitionName', (req = {}, res, next) => {
+  console.log(req.params.exhibitionName);
+  Exhibition.getExhibition(req.params.eventName, req.params.exhibitionName, (err, exhibitionObj) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json('Unable to fetch data!');
+      next();
+    } else if (exhibitionObj) {
+      res.status(200).json({
+        id: exhibitionObj._id,
+        exhibitionName: exhibitionObj.exhibition_name,
+        exhibitionDescription: exhibitionObj.exhibition_description,
+        eventName: exhibitionObj.event_name,
+        website: exhibitionObj.website,
+        poster: exhibitionObj.poster,
+        images: exhibitionObj.images,
+        videos: exhibitionObj.videos,
+        tags: exhibitionObj.tags,
+      });
+      next();
+    } else {
+      res.status(404).json('Nothing found!');
+      next();
+    }
+  });
+});
+
 
 module.exports = router;
