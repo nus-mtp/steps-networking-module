4580435b1ec4560f225639b908537eca20b4236a
Sean <sean@u.nus.edu>
2017-03-21 17:48:05 +0800

    Added route in Exhibition to set the tags of a specified Exhibition.

--- server/routes/exhibition.js
+++ server/routes/exhibition.js
@@ -79,5 +79,32 @@ router.get('/get/oneExhibition/:eventName/:exhibitionName', (req = {}, res, next
   });
 });
 
+// The Routes below utilize Comma-Separated Strings for the second argument in the Post Request
+// Use <Array>.toString() to generate a Comma-Separated String from an Array
+
+router.post('/post/oneExhibition/set/tags', (req = {}, res, next) => {
+  if (req.body && req.body.eventName && req.body.exhibitionName && req.body.tags) {
+    Exhibition.setTagsForExhibition(
+            req.body.eventName, req.body.exhibitionName, req.body.tags.split(','), (err, exhibition) => {
+              if (err) {
+                if (err.name === 'ValidationError') {
+                  console.log(err);
+                  res.status(403).json('Unauthorized!');
+                } else {
+                  console.log(err);
+                  res.status(500).json('Unable to post data!');
+                }
+              } else if (exhibition) {
+                res.status(200).json(extractExhibitionInfo(exhibition));
+              } else {
+                res.status(404).json('Nothing found!');
+              }
+              next();
+            });
+  } else {
+    res.status(400).json('Bad Request!');
+    next();
+  }
+});
 
 module.exports = router;
