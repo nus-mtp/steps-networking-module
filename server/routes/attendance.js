986da9ea50103547326ce1b84047d4dcb5b0272d
Sean <sean@u.nus.edu>
2017-03-22 21:07:48 +0800

    Added an Attendance route for obtaining all Attendances a User has been registered with.

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,25 +1,69 @@
+const async = require('async');
 const express = require('express');
 
 const router = new express.Router();
 
+const Event = require('../database/objectClasses/Event');
+const Exhibition = require('../database/objectClasses/Exhibition');
 const Attendance = require('../database/objectClasses/Attendance');
 
+const extractEventInfo = require('../utils/utils').extractEventInfo;
+const extractExhibitionInfo = require('../utils/utils').extractExhibitionInfo;
 const extractAttendanceInfo = require('../utils/utils').extractAttendanceInfo;
 
 // All Routes prefixed with 'attendance/'
 // Note: An Attendance Document tracks the Event or Exhibition ID
 
+// Get all the Events and Exhibitions that a User is participating in
 router.get('/get/oneUserAttendances/:email', (req = {}, res, next) => {
   if (req.params && req.params.email) {
     Attendance.searchAttendancesByUser(req.params.email, (err, attendances) => {
       if (err) {
         res.status(500).json('Unable to fetch data!');
+        next();
       } else if (attendances) {
-        res.status(200).json(attendances.map(attendance => extractAttendanceInfo(attendance)));
+        async.map(attendances,
+            (attendance, callback) => {
+              if (attendance) {
+                const attendanceKey = attendance.attendance_key;
+                const attendanceType = attendance.attendance_type;
+
+                if (attendanceType === 'event') {
+                  Event.getEventById(attendanceKey, (err, event) => {
+                    if (err || !event) {
+                      callback(null, null);
+                    } else {
+                      callback(null, extractEventInfo(event));
+                    }
+                  });
+                } else if (attendanceType === 'exhibition') {
+                  Exhibition.getExhibitionById(attendanceKey, (err, exhibition) => {
+                    if (err || !exhibition) {
+                      callback(null, null);
+                    } else {
+                      callback(null, extractExhibitionInfo(exhibition));
+                    }
+                  });
+                } else {
+                  callback(null, null);
+                }
+              } else {
+                callback(null, null);
+              }
+            },
+            (err, results) => {
+              if (err) {
+                res.status(500).json('Unable to process data!');
+                next();
+              } else {
+                res.status(200).json(results.filter(item => (item !== null)));
+                next();
+              }
+            });
       } else {
         res.status(404).json('Nothing found!');
+        next();
       }
-      next();
     });
   } else {
     res.status(400).json('Bad Request!');
