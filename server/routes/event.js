0695855af4e275e75af924d449958ca22ddd41fc
Jean <jkohx3@gmail.com>
2017-03-22 14:48:09 +0800

    Added tags as a returned parameter. Added 3 updates post methods for map, eventpicture and tags

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const router = new express.Router();
-
 const Event = require('../database/objectClasses/Event');
 
 router.get('/get/allEvents', (req = {}, res, next) => {
@@ -19,6 +17,7 @@ router.get('/get/allEvents', (req = {}, res, next) => {
         description: eventObj.event_description,
         event_poster: eventObj.event_picture,
         siteMap: eventObj.map,
+        tags: eventObj.tags,
       })));
       next();
     } else {
@@ -43,6 +42,7 @@ router.get('/get/oneEvent/:eventName', (req = {}, res, next) => {
         description: eventObj.event_description,
         event_poster: eventObj.event_picture,
         siteMap: eventObj.map,
+        tags: eventObj.tags,
       });
       next();
     } else {
@@ -74,6 +74,59 @@ router.get('/get/searchTag/:tag', (req = {}, res, next) => {
       next();
     }
   });
+});
+
+router.post('/post/updateMap', (req = {}, res, next) => {
+  if (req.body && req.body.eventName && req.body.map){
+    Event.updateEventMap(req.body.eventName, req.body.map, (err, results) => {
+      if (err) {
+        res.status(500).json('Unable to save data!');
+      } else if (results) {
+        res.status(200).send('Added!');
+      } else {
+        res.status(404).json('Message object not found!');
+      }
+      next();
+    });
+  } else {
+    res.status(400).send('Bad request!');
+  }
+});
+
+router.post('/post/updateEventPicture', (req = {}, res, next) => {
+  if (req.body && req.body.eventName && req.body.eventPicture){
+    Event.updateEventPicture(req.body.eventName, req.body.eventPicture, (err, results) => {
+      if (err) {
+        res.status(500).json('Unable to save data!');
+      } else if (results) {
+        res.status(200).send('Added!');
+      } else {
+        res.status(404).json('Message object not found!');
+      }
+      next();
+    });
+  } else {
+    res.status(400).send('Bad request!');
+    next();
+  }
+});
+
+router.post('/post/updateTags', (req = {}, res, next) => {
+  if (req.body && req.body.eventName && req.body.tags){
+    Event.updateEventPicture(req.body.eventName, req.body.tags.split(','), (err, results) => {
+      if (err) {
+        res.status(500).json('Unable to save data!');
+      } else if (results) {
+        res.status(200).send('Added!');
+      } else {
+        res.status(404).json('Message object not found!');
+      }
+      next();
+    });
+  } else {
+    res.status(400).send('Bad request!');
+    next();
+  }
 });
 
 module.exports = router;
