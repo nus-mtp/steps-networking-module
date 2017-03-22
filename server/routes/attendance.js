b93f90e511835c446370b659277d9c2198a07d0c
Sean <sean@u.nus.edu>
2017-03-22 23:03:05 +0800

    Add an Attendance route for obtaining all Events a User is attending.

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -71,6 +71,56 @@ router.get('/get/oneUserAttendances/:email', (req = {}, res, next) => {
   }
 });
 
+// Get all Events a User is attending / has attended
+router.get('/get/oneUserEventAttendances/:email', (req = {}, res, next) => {
+  if (req.params && req.params.email) {
+    Attendance.searchAttendancesByUser(req.params.email, (err, attendances) => {
+      if (err) {
+        res.status(500).json('Unable to fetch data!');
+        next();
+      } else if (attendances) {
+        async.mapLimit(attendances, 5,
+            (attendance, callback) => {
+              // Limit number of concurrent connections made by this request to 5
+              if (attendance) {
+                const attendanceKey = attendance.attendance_key;
+                const attendanceType = attendance.attendance_type;
+
+                if (attendanceType === 'event') {
+                  Event.getEventById(attendanceKey, (err, event) => {
+                    if (err || !event) {
+                      callback(null, null);
+                    } else {
+                      callback(null, extractEventInfo(event));
+                    }
+                  });
+                } else {
+                  callback(null, null);
+                }
+              } else {
+                callback(null, null);
+              }
+            },
+            (err, results) => {
+              if (err || !results) {
+                res.status(500).json('Unable to process data!');
+                next();
+              } else {
+                res.status(200).json(results.filter(item => (item !== null)));
+                next();
+              }
+            });
+      } else {
+        res.status(404).json('Nothing found!');
+        next();
+      }
+    });
+  } else {
+    res.status(400).json('Bad Request!');
+    next();
+  }
+});
+
 // Get all the Exhibitions a User is participating in / has participated in under one Event
 router.get('/get/oneUserAttendancesForEvent/:email/:eventName', (req = {}, res, next) => {
   if (req.params && req.params.email && req.params.eventName) {
