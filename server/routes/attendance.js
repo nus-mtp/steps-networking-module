63bc7da04671385dd809d9235b554de5fba8db7a
Sean <sean@u.nus.edu>
2017-03-22 22:49:14 +0800

    Added Attendance route for obtaining Exhibitions a User has participated in under one Event.

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -12,9 +12,8 @@ const extractExhibitionInfo = require('../utils/utils').extractExhibitionInfo;
 const extractAttendanceInfo = require('../utils/utils').extractAttendanceInfo;
 
 // All Routes prefixed with 'attendance/'
-// Note: An Attendance Document tracks the Event or Exhibition ID
 
-// Get all the Events and Exhibitions that a User is participating in
+// Get all the Events and Exhibitions that a User is participating in / has participated in
 router.get('/get/oneUserAttendances/:email', (req = {}, res, next) => {
   if (req.params && req.params.email) {
     Attendance.searchAttendancesByUser(req.params.email, (err, attendances) => {
@@ -53,7 +52,59 @@ router.get('/get/oneUserAttendances/:email', (req = {}, res, next) => {
               }
             },
             (err, results) => {
-              if (err) {
+              if (err || !results) {
+                res.status(500).json('Unable to process data!');
+                next();
+              } else {
+                res.status(200).json(results.filter(item => (item !== null)));
+                next();
+              }
+            });
+      } else {
+        res.status(404).json('Nothing found!');
+        next();
+      }
+    });
+  } else {
+    res.status(400).json('Bad Request!');
+    next();
+  }
+});
+
+// Get all the Exhibitions a User is participating in / has participated in under one Event
+router.get('/get/oneUserAttendancesForEvent/:email/:eventName', (req = {}, res, next) => {
+  if (req.params && req.params.email && req.params.eventName) {
+    Attendance.searchAttendancesByUser(req.params.email, (err, attendances) => {
+      if (err) {
+        res.status(500).json('Unable to fetch data!');
+        next();
+      } else if (attendances) {
+        async.mapLimit(attendances, 5,
+            (attendance, callback) => {
+              // Limit number of concurrent connections made by this request to 5
+              if (attendance) {
+                const attendanceKey = attendance.attendance_key;
+                const attendanceType = attendance.attendance_type;
+
+                if (attendanceType === 'exhibition') {
+                  Exhibition.getExhibitionById(attendanceKey, (err, exhibition) => {
+                    if (err || !exhibition) {
+                      callback(null, null);
+                    } else if (exhibition.event_name === req.params.eventName) {
+                      callback(null, extractExhibitionInfo(exhibition));
+                    } else {
+                      callback(null, null);
+                    }
+                  });
+                } else {
+                  callback(null, null);
+                }
+              } else {
+                callback(null, null);
+              }
+            },
+            (err, results) => {
+              if (err || !results) {
                 res.status(500).json('Unable to process data!');
                 next();
               } else {
