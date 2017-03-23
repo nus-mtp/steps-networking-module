1454650b651bb40144249892f2618af07c4c3c70
Jean <jkohx3@gmail.com>
2017-03-23 12:39:20 +0800

    Updated sending message to make it easier to debug

--- server/routes/message.js
+++ server/routes/message.js
@@ -8,7 +8,7 @@ router.get('/get/getMessages/:senderEmail/:recipientEmail', (req = {}, res, next
   if (req.params && req.params.senderEmail && req.params.recipientEmail) {
     Message.getConversation(req.params.senderEmail, req.params.recipientEmail, (err, msgObjs) => {
       if (err) {
-        res.status(500).json('Unable to fetch data!');
+        res.status(500).json('Unable to fetch data from '+req.params.senderEmail+ ' to '+ req.params.recipientEmail);
       } else if (msgObjs) {
         res.status(200).json({
           sender: msgObjs.sender_email,
@@ -16,12 +16,12 @@ router.get('/get/getMessages/:senderEmail/:recipientEmail', (req = {}, res, next
           messages: msgObjs.messages,
         });
       } else {
-        res.status(404).json('Not found!');
+        res.status(404).json('Message object from '+ req.params.senderEmail+ ' to '+ req.params.recipientEmail+ ' was not found!');
       }  
       next();
     });
   } else {
-    res.status(400).json('Bad request!');
+    res.status(400).json('Bad request! "recipientEmail".');
     next();
   }
 });
@@ -36,12 +36,12 @@ router.get('/get/getMessagesFrom/:senderEmail', (req = {}, res, next) => {
           emails: msgObj.recipient_email,
         })));
       } else {
-        res.status(404).json('Not found!');
+        res.status(404).json('Message object from '+ req.params.senderEmail+ ' was not found!');
       }
       next();
     });
   } else {
-    res.status(400).json('Bad request!');
+    res.status(400).json('Bad request! Expecting "senderEmail".');
     next();
   }
 });
@@ -50,19 +50,19 @@ router.get('/get/getMessagesTo/:recipientEmail', (req = {}, res, next) => {
   if (req.params && req.params.recipientEmail) {
     Message.getMessagesToUser(req.params.recipientEmail, (err, msgObjs) => {
       if (err) {
-        res.status(500).json('Unable to fetch data!');
+        res.status(500).json('Unable to fetch data from '+req.params.recipientEmail );
       } else if (msgObjs[0]) {
         
         res.status(200).json(msgObjs.map(msgObj => ({
           emails: msgObj.sender_email,
         })));
       } else {
-        res.status(404).json('Not found!');
+        res.status(404).json('Message object to '+req.params.recipientEmail+' was not found!');
       }
       next();
     });
   } else {
-    res.status(400).json('Bad request!');
+    res.status(400).json('Bad request!Expecting "recipientEmail".');
     next();
   }
 });
@@ -71,16 +71,16 @@ router.get('/get/getMessagesInvolving/:userEmail', (req = {}, res, next) => {
   if (req.params && req.params.userEmail) {
     Message.getEmailsInvolvingUser(req.params.userEmail, (err, msgObjs) => {
       if (err) {
-        res.status(500).json('Unable to fetch data!');
+        res.status(500).json('Unable to fetch data involving '+req.params.userEmail);
       } else if (msgObjs[0]) {
         res.status(200).json(msgObjs);
       } else {
-        res.status(404).json('Not found!');
+        res.status(404).json(req.params.userEmail + ' not found!');
       }
       next();
     });
   } else {
-    res.status(400).json('Bad request!');
+    res.status(400).json('Bad request! Expecting "userEmail".');
     next();
   }
 });
@@ -91,7 +91,7 @@ router.post('/post/newMessage', (req = {}, res, next) => {
     newMessage.saveMessage((err) => {
       if (err) {
         if ( err.code == 11000 ) {
-          res.status(500).json('Message object already exists!');
+          res.status(500).json('Message object from '+ req.body.senderEmail +' to '+req.body.recipientEmail + ' already exists!');
         } else {
           res.status(500).json('Unable to save data!');
         }
@@ -101,7 +101,7 @@ router.post('/post/newMessage', (req = {}, res, next) => {
       next();
     });
   } else {
-    res.status(400).send('Bad request!');
+    res.status(400).send('Bad request! Expecting "senderEmail", "recipientEmail" and "content".');
     next();
   }
 });
@@ -110,7 +110,7 @@ router.post('/post/addMessage', (req = {}, res, next) => {
   if (req.body.senderEmail && req.body.recipientEmail && req.body.content){
     Message.addMessage(req.body.senderEmail, req.body.recipientEmail, req.body.content, Date.now(), (err, results) => {
       if (err) {
-        res.status(500).json('Unable to save data!');
+        res.status(500).json('Unable to save data from '+req.body.senderEmail+' to '+req.body.recipientEmail);
         next();
       } else if (results) {
         res.status(200).send('Added!');
@@ -119,7 +119,7 @@ router.post('/post/addMessage', (req = {}, res, next) => {
         const newMessage = new Message(req.body.senderEmail, req.body.recipientEmail, req.body.content, Date.now());
         newMessage.saveMessage((err) => {
           if (err) {
-            res.status(500).json('Unable to save data!');
+            res.status(500).json('Unable to save data from'+req.body.senderEmail+' to ' +req.body.recipientEmail);
             next();
           } else {
             res.status(200).send('New Message Object made');
@@ -129,7 +129,7 @@ router.post('/post/addMessage', (req = {}, res, next) => {
       }
     });
   } else {
-    res.status(400).send('Bad request!');
+    res.status(400).send('Bad request! Expecting "senderEmail", "recipientEmail" and "content".');
     next();
   }
 });
