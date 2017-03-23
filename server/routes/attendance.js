c86ab53dc21896b3b62b9dbd37df8143c46cda20
Sean <sean@u.nus.edu>
2017-03-23 10:49:25 +0800

    Added a new Attendance route for get the participants of a Exhibition.

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -3,10 +3,12 @@ const express = require('express');
 
 const router = new express.Router();
 
+const User = require('../database/objectClasses/User');
 const Event = require('../database/objectClasses/Event');
 const Exhibition = require('../database/objectClasses/Exhibition');
 const Attendance = require('../database/objectClasses/Attendance');
 
+const extractUserInfo = require('../utils/utils').extractUserInfo;
 const extractEventInfo = require('../utils/utils').extractEventInfo;
 const extractExhibitionInfo = require('../utils/utils').extractExhibitionInfo;
 const extractAttendanceInfo = require('../utils/utils').extractAttendanceInfo;
@@ -164,6 +166,55 @@ router.get('/get/oneUserAttendancesForEvent/:email/:eventName', (req = {}, res, 
             });
       } else {
         res.status(404).json('Nothing found!');
+        next();
+      }
+    });
+  } else {
+    res.status(400).json('Bad Request!');
+    next();
+  }
+});
+
+// Get the Users participating of an Exhibition
+router.get('/get/oneExhibitionParticipants/:eventName/:exhibitionName', (req = {}, res, next) => {
+  if (req.params && req.params.eventName && req.params.exhibitionName) {
+    Exhibition.getExhibition(req.params.eventName, req.params.exhibitionName, (err, exhibition) => {
+      if (err) {
+        res.status(500).json('Unable to fetch data!');
+        next();
+      } else if (exhibition) {
+        Attendance.searchAttendancesByKey(exhibition._id, (err, attendances) => {
+          if (err) {
+            res.status(500).json('Unable to fetch data!');
+            next();
+          } else if (attendances) {
+            async.mapLimit(attendances, 5,
+                (attendance, callback) => {
+                  // Limit number of concurrent connections made by this request to at most 5
+                  User.getUser(attendance.user_email, (err, user) => {
+                    if (err || !user) {
+                      callback(null, null);
+                    } else {
+                      callback(null, user);
+                    }
+                  });
+                },
+                (err, results) => {
+                  if (err || !results) {
+                    res.status(500).json('Unable to process data!');
+                    next();
+                  } else {
+                    res.status(200).json(results.filter(item => (item !== null)));
+                    next();
+                  }
+                });
+          } else {
+            res.status(404).json('Cannot find any Participants for this Exhibition!');
+            next();
+          }
+        });
+      } else {
+        res.status(404).json('Unable to find Exhibition!');
         next();
       }
     });
