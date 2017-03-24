fcedcbe3ba123bd9be50f2ba287828a5b783df61
Sean <sean@u.nus.edu>
2017-03-24 19:56:12 +0800

    Changed type for bookmarked users to be ObjectID, specified reference to be the user model.

--- server/database/schemas/ourSchemas/user.js
+++ server/database/schemas/ourSchemas/user.js
@@ -38,8 +38,8 @@ const userSchema = new mongoose.Schema({
   ],
   bookmarked_users: [
     {
-      type: String,
-      trim: true,
+      type: mongoose.Schema.ObjectId,
+      ref: 'user',
       unique: true,
     },
   ],
