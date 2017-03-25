eb90c3342ba16e291d1d1d3da977ca83caf5fc40
Sean <sean@u.nus.edu>
2017-03-25 21:24:07 +0800

    Altered Comment objectClass methods to use a global db connection pool rather than creating and destroying their own with each method call.

--- server/database/objectClasses/Comment.js
+++ server/database/objectClasses/Comment.js
@@ -1,14 +1,5 @@
 const ModelHandler = require('../models/ourModels.js');
 
-const config = require('../../config.json');
-const currentdb = require('../../currentdb.js');
-
-const username = config[currentdb].username;
-const password = config[currentdb].password;
-const host = config[currentdb].host;
-const port = config[currentdb].port;
-const dbName = config[currentdb].database;
-
 /**
  * This is the wrapper class used extract out and store information
  * about the comments from the Database between view and model
@@ -16,23 +7,29 @@ const dbName = config[currentdb].database;
  */
 class Comment {
   /**
-   * Creates a connection to the Database.
+   * Establishes the Comment Model on an existing connection.
+   *
+   * @param {Mongoose.Connection} db: The connection to the db.
    */
-  static connectDB() {
-    this.ModelHandler = new ModelHandler()
-        .initWithParameters(username, password, host, port, dbName);
-    this.CommentModel = this.ModelHandler.getCommentModel();
+  static setDBConnection(db) {
+    if (!Comment.db || !Comment.CommentModel) {
+      Comment.db = db;
+      Comment.CommentModel = new ModelHandler().initWithConnection(db).getCommentModel();
+    }
   }
 
   /**
-   * Disconnect from database.
+   * A function which checks whether the Database connection can be used.
+   *
+   * @returns {Mongoose.Connection|*|*|Aggregate|Model|boolean}
    */
-  static disconnectDB() {
-    this.ModelHandler.disconnect();
+  static checkConnection() {
+    return (Comment.db && Comment.CommentModel &&
+      (Comment.db.readyState === 1 || Comment.db.readyState === 2));
   }
 
   /**
-   * Creates a Comment Document and stores it internally.
+   * Creates a Comment JSON and stores it internally.
    *
    * @param {String} userEmail: The email of the User that made the Comment.
    * @param {mongoose.Schema.ObjectId} exhibitionKey: The ObjectId of the Exhibition.
@@ -40,28 +37,27 @@ class Comment {
    * @param {Date} date: Date object to indicate when the Comment was made.
    */
   constructor(userEmail, exhibitionKey, comment, date) {
-    this.ModelHandler = new ModelHandler()
-        .initWithParameters(username, password, host, port, dbName);
-    this.CommentModel = this.ModelHandler.getCommentModel();
-    this.commentModelDoc = new this.CommentModel({
+    this.commentJSON = {
       user_email: userEmail,
       exhibition_key: exhibitionKey,
       comments: { content: comment, timestamp: date },
-    });
-    this.ModelHandler.disconnect();
+    };
   }
 
   /**
-   * Saves the Comment Document stored internally to the Database.
+   * Saves the Comment JSON to the Database as an actual Document.
    *
    * @param {function} callback: A function that is executed once the operation is done.
    */
   saveComment(callback) {
-    Comment.connectDB();
-    this.commentModelDoc.save((err, result) => {
-      Comment.disconnectDB();
-      callback(err, result);
-    });
+    if (Comment.checkConnection()) {
+      const commentDoc = new Comment.CommentModel(this.commentJSON);
+      commentDoc.save((err, result) => {
+        callback(err, result);
+      });
+    } else {
+      callback(null);
+    }
   }
 
   /**
@@ -74,17 +70,19 @@ class Comment {
    * @param {function} callback: A function that is executed once the operation is done.
    */
   static addCommentForExhibition(userEmail, exhibitionKey, comment, date, callback) {
-    Comment.connectDB();
-    const query = {
-      user_email: userEmail,
-      exhibition_key: exhibitionKey,
-    };
-    const update = { $push: { comments: { content: comment, timestamp: date } } };
-    const options = { new: true };
-    this.CommentModel.findOneAndUpdate(query, update, options, (err, results) => {
-      Comment.disconnectDB();
-      callback(err, results);
-    });
+    if (Comment.checkConnection()) {
+      const query = {
+        user_email: userEmail,
+        exhibition_key: exhibitionKey,
+      };
+      const update = { $push: { comments: { content: comment, timestamp: date } } };
+      const options = { new: true };
+      Comment.CommentModel.findOneAndUpdate(query, update, options, (err, results) => {
+        callback(err, results);
+      });
+    } else {
+      callback('Not Connected!', null);
+    }
   }
 
     /**
@@ -95,17 +93,19 @@ class Comment {
    * @param {function} callback: A function that is executed once the operation is done.
    */
   static getUserCommentsForExhibition(userEmail, exhibitionKey, callback) {
-    const query = {
-      user_email: userEmail,
-      exhibition_key: exhibitionKey,
+    if (Comment.checkConnection()) {
+      const query = {
+        user_email: userEmail,
+        exhibition_key: exhibitionKey,
+      };
+      Comment.CommentModel.findOne(query, (err, matchedComments) => {
+        callback(err, matchedComments);
+      });
+    } else {
+      callback('Not Connected!', null);
     }
-    Comment.connectDB();
-    this.CommentModel.findOne(query, (err, matchedComments) => {
-      Comment.disconnectDB();
-      callback(err, matchedComments);
-    });
   }
-  
+
   /**
    * Retrieve list of Comments for a specific Exhibition.
    *
@@ -113,11 +113,13 @@ class Comment {
    * @param {function} callback: A function that is executed once the operation is done.
    */
   static getCommentsForExhibition(exhibitionKey, callback) {
-    Comment.connectDB();
-    this.CommentModel.find({ exhibition_key: exhibitionKey }, (err, matchedComments) => {
-      Comment.disconnectDB();
-      callback(err, matchedComments);
-    });
+    if (Comment.checkConnection()) {
+      Comment.CommentModel.find({ exhibition_key: exhibitionKey }, (err, matchedComments) => {
+        callback(err, matchedComments);
+      });
+    } else {
+      callback('Not Connected!', null);
+    }
   }
 
   /**
@@ -127,11 +129,13 @@ class Comment {
    * @param {function} callback: A function that is executed once the operation is done.
    */
   static clearCommentsForExhibition(exhibitionKey, callback) {
-    Comment.connectDB();
-    this.CommentModel.find({ exhibition_key: exhibitionKey }).remove().exec((err, results) => {
-      Comment.disconnectDB();
-      callback(err, results);
-    });
+    if (Comment.checkConnection()) {
+      Comment.CommentModel.find({ exhibition_key: exhibitionKey }).remove().exec((err, results) => {
+        callback(err, results);
+      });
+    } else {
+      callback('Not Connected!', null);
+    }
   }
 
   /**
@@ -140,11 +144,13 @@ class Comment {
    * @param {function} callback: A function that is executed once the operation is done.
    */
   static clearAllComments(callback) {
-    Comment.connectDB();
-    this.CommentModel.collection.remove({}, (err) => {
-      Comment.disconnectDB();
-      callback(err);
-    });
+    if (Comment.checkConnection()) {
+      Comment.CommentModel.collection.remove({}, (err) => {
+        callback(err);
+      });
+    } else {
+      callback('Not Connected!', null);
+    }
   }
 
 }
