ae87971814978ad2114d3b548538916d09b51735
Sean <sean@u.nus.edu>
2017-03-25 10:31:17 +0800

    Updated User test to use Promise for User Update.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,17 +1,18 @@
+const async = require('async');
 const User = require('../server/database/objectClasses/User.js');
 const assert = require('assert');
 
 describe('User Create', () => {
   before((done) => {
     const userTest2 = new User(
-      'usertesting_2@user.com',
-      'UserTest2',
-      'I am the second test user.',
-      'password456', false, false,
-      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
-      ['Producing skills', 'Photoshop', 'Illustrator', 'AutoCAD', 'Microsoft Office'],
-      [],
-    );
+            'usertesting_2@user.com',
+            'UserTest2',
+            'I am the second test user.',
+            'password456', false, false,
+            'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
+            ['Producing skills', 'Photoshop', 'Illustrator', 'AutoCAD', 'Microsoft Office'],
+            [],
+        );
     userTest2.saveUser((err) => {
       if (err) {
         console.log(err);
@@ -31,16 +32,16 @@ describe('User Create', () => {
 
   it('Should be able to add new Users', (done) => {
     const userTest3 = new User(
-      'usertesting_3@user.com',
-      'UserTest3',
-      'I am the third test user.',
-      'password789',
-      true,
-      false,
-      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
-      ['Project Management', 'Programming skills', 'Objective C', 'C++', 'C#'],
-      [],
-    );
+            'usertesting_3@user.com',
+            'UserTest3',
+            'I am the third test user.',
+            'password789',
+            true,
+            false,
+            'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
+            ['Project Management', 'Programming skills', 'Objective C', 'C++', 'C#'],
+            [],
+        );
     userTest3.saveUser((err) => {
       if (err) {
         console.log('error with saving user3');
@@ -60,19 +61,19 @@ describe('User Create', () => {
 
   it('Should not be able to add Users with duplicate email', (done) => {
     const userTestDup = new User(
-      'usertesting_2@user.com',
-      'UserTest2 again~!',
-      'I am the duplicated second test user.',
-      'sadawe123',
-      false,
-      false,
-      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
-      ['Project Management', 'Programming skills', 'Objective C', 'C++', 'C#'],
-      [, 'usertesting_2@user.com'],
-    );
+            'usertesting_2@user.com',
+            'UserTest2 again~!',
+            'I am the duplicated second test user.',
+            'sadawe123',
+            false,
+            false,
+            'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
+            ['Project Management', 'Programming skills', 'Objective C', 'C++', 'C#'],
+            [],
+        );
     userTestDup.saveUser((err) => {
       if (err) {
-        // duplicate error expected
+                // duplicate error expected
       } else {
         User.getUser('usertesting_2@user.com', (err1, results) => {
           if (err1) {
@@ -92,16 +93,16 @@ describe('User Create', () => {
 describe('User Read', () => {
   before((done) => {
     const userTest1 = new User(
-      'usertesting_1@user.com', // email
-      'UserTest1', // name
-      'I am the first test user.', // description
-      'password123', // password
-      true, // will_notify
-      false, // is_deleted
-      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // profile_pic
-      ['Programming skills', 'C++', 'Java', 'HTML'], // skills
-      [], // bookmarked users
-    );
+            'usertesting_1@user.com', // email
+            'UserTest1', // name
+            'I am the first test user.', // description
+            'password123', // password
+            true, // will_notify
+            false, // is_deleted
+            'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // profile_pic
+            ['Programming skills', 'C++', 'Java', 'HTML'], // skills
+            [], // bookmarked users
+        );
     userTest1.saveUser((err) => {
       if (err) {
         console.log(err);
@@ -138,7 +139,7 @@ describe('User Read', () => {
     User.getUser('usertesting_4@user.com', (err, userObj) => {
       if (err) {
         console.log('Something went wrong with getting user function');
-        // console.log(err);
+                // console.log(err);
       } else {
         assert.equal(null, userObj);
       }
@@ -146,7 +147,6 @@ describe('User Read', () => {
     });
   });
 });
-
 
 describe('User Update', () => {
   before((done) => {
@@ -162,27 +162,45 @@ describe('User Update', () => {
       [], //bookmarked users
     );
     const userTest2 = new User(
-        'usertesting_2@user.com', // email
-        'UserTest2', // name
-        'I am the second test user.', // description
-        'password123', // password
-        true, // will_notify
-        false, // is_deleted
-        'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // profile_pic
-        ['Programming skills', 'C++', 'Java', 'HTML'], // skills
-        [], //bookmarked users
+      'usertesting_2@user.com', // email
+      'UserTest2', // name
+      'I am the second test user.', // description
+      'password123', // password
+      true, // will_notify
+      false, // is_deleted
+      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png', // profile_pic
+      ['Programming skills', 'C++', 'Java', 'HTML'], // skills
+      [], //bookmarked users
     );
-    userTest1.saveUser((err) => {
-      if (err) {
-        console.log(err);
-      }
-      userTest2.saveUser((err) => {
+
+    const save1 = new Promise((resolve, reject) => {
+      userTest1.saveUser((err) => {
         if (err) {
-          console.log(err);
+          reject(err);
+        } else {
+          resolve();
         }
-        done();
       });
     });
+
+    const save2 = new Promise((resolve, reject) => {
+      userTest2.saveUser((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    Promise.all([save1, save2])
+        .then(() => {
+          done();
+        })
+        .catch((reasons) => {
+          console.log(reasons);
+          done();
+        });
   });
 
   after((done) => {
@@ -192,6 +210,10 @@ describe('User Update', () => {
       }
       done();
     });
+  });
+
+  it('should be able to do nothing', (done) => {
+    done();
   });
 
   it('Should be able to update the profile picture of the user', (done) => {
@@ -307,59 +329,58 @@ describe('User Update', () => {
 
   it('Should be able to update an existing specific user', (done) => {
     User.updateUser(
-      'usertesting_1@user.com',
-      'User Test 1 Updated',
-      'my description is updated',
-      'changedPassword',
-      false,
-      false,
-      'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1',
-      ['c++', 'C#', 'java'],
-      [],
-      (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          assert.notEqual(results, null);
-          assert.equal('my description is updated', results.description);
-        }
-        done();
-      });
+            'usertesting_1@user.com',
+            'User Test 1 Updated',
+            'my description is updated',
+            'changedPassword',
+            false,
+            false,
+            'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1',
+            ['c++', 'C#', 'java'],
+            [],
+            (err, results) => {
+              if (err) {
+                console.log(err);
+              } else {
+                assert.notEqual(results, null);
+                assert.equal('my description is updated', results.description);
+              }
+              done();
+            });
   });
 
   it('Should not be able to update a non-existing user', (done) => {
     User.updateUser(
-      'user2000@user.com',
-      'i am a duplicate',
-      'my descript is useless',
-      'qwe141asdasd1',
-      true,
-      false,
-      'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1',
-      ['c++', 'C#', 'java'],
-      [],
-      (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          assert.equal(null, results);
-          done();
-        }
-      });
+            'user2000@user.com',
+            'i am a duplicate',
+            'my descript is useless',
+            'qwe141asdasd1',
+            true,
+            false,
+            'https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2Fp7HfgdZ.png&f=1',
+            ['c++', 'C#', 'java'],
+            [],
+            (err, results) => {
+              if (err) {
+                console.log(err);
+              } else {
+                assert.equal(null, results);
+              }
+              done();
+            });
   });
 });
-
 
 describe('User Delete', () => {
   before((done) => {
     const userTest2 = new User(
-      'usertesting_2@user.com',
-      'UserTest2',
-      'I am the second test user.',
-      'password456', false, false,
-      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
-      ['Producing skills', 'Photoshop', 'Illustrator', 'AutoCAD', 'Microsoft Office'],
-      []);
+            'usertesting_2@user.com',
+            'UserTest2',
+            'I am the second test user.',
+            'password456', false, false,
+            'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png',
+            ['Producing skills', 'Photoshop', 'Illustrator', 'AutoCAD', 'Microsoft Office'],
+            []);
     userTest2.saveUser((err) => {
       if (err) {
         console.log(err);
