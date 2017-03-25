87d26ef1f2be5bb2fda3523b975ec336babb580c
Sean <sean@u.nus.edu>
2017-03-25 18:20:47 +0800

    BUGFIX: Shifted all app initialization into the openCallback of the accessMongoDB function.

--- server/server.js
+++ server/server.js
@@ -15,34 +15,6 @@ app.use('/js', express.static(path.join(`${__dirname}/../node_modules/bootstrap/
 app.use('/tether', express.static(path.join(`${__dirname}/../node_modules/tether/dist/js`)));  // redirect tether
 app.use('/jquery', express.static(path.join(`${__dirname}/../node_modules/jquery/dist`)));  // redirect jquery
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../app/index.html`));   // It will find and locate index.html from View or Scripts
-});
-
-const authCheckMiddleware = require('./middleware/auth-check');
-
-app.use('/api', authCheckMiddleware);
-
-const authRoutes = require('./routes/auth');
-const apiRoutes = require('./routes/api');
-
-app.use('/auth', authRoutes);
-app.use('/api', apiRoutes);
-
-const userRoutes = require('./routes/user');
-const eventRoutes = require('./routes/event');
-const exhibitionRoutes = require('./routes/exhibition');
-const attendanceRoutes = require('./routes/attendance');
-const commentRoutes = require('./routes/comment');
-const messageRoutes = require('./routes/message');
-
-app.use('/user', userRoutes);
-app.use('/event', eventRoutes);
-app.use('/exhibition', exhibitionRoutes);
-app.use('/attendance', attendanceRoutes);
-app.use('/comment', commentRoutes);
-app.use('/message', messageRoutes);
-
 const db = require('./database/mongodbScripts/accessMongoDB').connect(
     config[currentdb].username, config[currentdb].password, config[currentdb].host,
     config[currentdb].port, config[currentdb].database, 30,
@@ -53,6 +25,10 @@ const db = require('./database/mongodbScripts/accessMongoDB').connect(
 
       app.locals.db = db;
 
+      app.get('/', (req, res) => {
+        res.sendFile(path.join(`${__dirname}/../app/index.html`));   // It will find and locate index.html from View or Scripts
+      });
+
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: false }));
       app.use(passport.initialize());
@@ -62,6 +38,30 @@ const db = require('./database/mongodbScripts/accessMongoDB').connect(
 
       passport.use('local-signup', localSignUpStrategy);
       passport.use('local-login', localLoginStrategy);
+
+      const authCheckMiddleware = require('./middleware/auth-check');
+
+      app.use('/api', authCheckMiddleware);
+
+      const authRoutes = require('./routes/auth');
+      const apiRoutes = require('./routes/api');
+
+      app.use('/auth', authRoutes);
+      app.use('/api', apiRoutes);
+
+      const userRoutes = require('./routes/user');
+      const eventRoutes = require('./routes/event');
+      const exhibitionRoutes = require('./routes/exhibition');
+      const attendanceRoutes = require('./routes/attendance');
+      const commentRoutes = require('./routes/comment');
+      const messageRoutes = require('./routes/message');
+
+      app.use('/user', userRoutes);
+      app.use('/event', eventRoutes);
+      app.use('/exhibition', exhibitionRoutes);
+      app.use('/attendance', attendanceRoutes);
+      app.use('/comment', commentRoutes);
+      app.use('/message', messageRoutes);
 
       app.listen(process.env.PORT || port, () => {
         const listeningPort = process.env.PORT || port;
