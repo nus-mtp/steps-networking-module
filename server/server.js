96ab109e48b8bbd8f3bed9ce47bd6e2e339010a3
Sean <sean@u.nus.edu>
2017-03-25 17:45:53 +0800

    Linted server.js.

--- server/server.js
+++ server/server.js
@@ -1,66 +1,66 @@
 import currentdb from './currentdb';
 
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var config = require('./config.json');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config.json');
 
-var db = require('./database/mongodbScripts/accessMongoDB').connect(config[currentdb].username,
+const db = require('./database/mongodbScripts/accessMongoDB').connect(config[currentdb].username,
                                                                     config[currentdb].password,
                                                                     config[currentdb].host,
                                                                     config[currentdb].port,
                                                                     config[currentdb].database);
 
-var app = express();
-var port = 3000;
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, '/../dist')));
-app.use('/css', express.static(path.join(__dirname + '/../node_modules/bootstrap/dist/css')));  // redirect CSS bootstrap
-app.use('/js', express.static(path.join(__dirname + '/../node_modules/bootstrap/dist/js')));  // redirect js bootstrap
-app.use('/tether', express.static(path.join(__dirname + '/../node_modules/tether/dist/js')));  // redirect tether
-app.use('/jquery', express.static(path.join(__dirname + '/../node_modules/jquery/dist')));  // redirect jquery
+app.use('/css', express.static(path.join(`${__dirname}/../node_modules/bootstrap/dist/css`)));  // redirect CSS bootstrap
+app.use('/js', express.static(path.join(`${__dirname}/../node_modules/bootstrap/dist/js`)));  // redirect js bootstrap
+app.use('/tether', express.static(path.join(`${__dirname}/../node_modules/tether/dist/js`)));  // redirect tether
+app.use('/jquery', express.static(path.join(`${__dirname}/../node_modules/jquery/dist`)));  // redirect jquery
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/../app/index.html'));   //It will find and locate index.html from View or Scripts
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../app/index.html`));   // It will find and locate index.html from View or Scripts
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
-var localSignUpStrategy = require('./passport/local-signup')(db);
-var localLoginStrategy = require('./passport/local-login')(db);
+const localSignUpStrategy = require('./passport/local-signup')(db);
+const localLoginStrategy = require('./passport/local-login')(db);
 passport.use('local-signup', localSignUpStrategy);
 passport.use('local-login', localLoginStrategy);
 
 const authCheckMiddleware = require('./middleware/auth-check');
 app.use('/api', authCheckMiddleware);
 
-var authRoutes = require('./routes/auth');
-var apiRoutes = require('./routes/api');
+const authRoutes = require('./routes/auth');
+const apiRoutes = require('./routes/api');
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
-var userRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 app.use('/user', userRoutes);
 
-var eventRoutes = require('./routes/event');
+const eventRoutes = require('./routes/event');
 app.use('/event', eventRoutes);
 
-var exhibitionRoutes = require('./routes/exhibition');
+const exhibitionRoutes = require('./routes/exhibition');
 app.use('/exhibition', exhibitionRoutes);
 
-var attendanceRoutes = require('./routes/attendance');
+const attendanceRoutes = require('./routes/attendance');
 app.use('/attendance', attendanceRoutes);
 
-var commentRoutes = require('./routes/comment');
+const commentRoutes = require('./routes/comment');
 app.use('/comment', commentRoutes);
 
-var messageRoutes = require('./routes/message');
+const messageRoutes = require('./routes/message');
 app.use('/message', messageRoutes);
 
-app.listen(process.env.PORT || port, function() {
-  var listeningPort = process.env.PORT || port;
+app.listen(process.env.PORT || port, () => {
+  const listeningPort = process.env.PORT || port;
   console.log(`Running on ${listeningPort}`);
 });
