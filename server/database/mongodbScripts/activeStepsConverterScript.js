cb30562963fd72cd34c18042ecc1bc408ac3642a
Sean <sean@u.nus.edu>
2017-03-26 21:04:46 +0800

    Made fullStepsConverterScript use the functionality inside stepsConverterUtil.

---
+++ server/database/mongodbScripts/activeStepsConverterScript.js
@@ -0,0 +1,19 @@
+/**
+ * This file contains a script to populate the a target database with
+ * information that can be used for our own App based on only the active
+ * Event information from the STePs DB.
+ * Ensure that both the src and dest MongoDB server connections are running before executing.
+ */
+const async = require('async');
+
+const converter = require('./stepsConverterUtil');
+
+const stepsEvent = converter.StepsModels.getEventModel();
+const stepsUser = converter.StepsModels.getUserModel();
+const stepsModule = converter.StepsModels.getModuleModel();
+const stepsGuest = converter.StepsModels.getGuestModel();
+
+const upsertEvent = converter.upsertEvent;
+const upsertUser = converter.upsertUser;
+const upsertModule = converter.upsertModule;
+const upsertGuest = converter.upsertGuest;
