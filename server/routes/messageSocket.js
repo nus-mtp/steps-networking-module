67014f72fb8b0522f34b0c62d8dabe8181ae2d93
Jean <jkohx3@gmail.com>
2017-03-29 18:01:46 +0800

    Updated methods to include get message, add message, get message involving user. These methods uses the message object classes

--- server/routes/messageSocket.js
+++ server/routes/messageSocket.js
@@ -4,35 +4,74 @@ exports = module.exports = function(io, db) {
   let ModelHandlerObj = new ModelHandler().initWithConnection(db);
 
   Message.setDBConnection(ModelHandlerObj.getConnection());
-
+  var socketIDs = {};
 
   io.on('connection', (socket) => {
-    socket.emit('news', {hi: 'hi'});
-    socket.on('newMessage',(data) => {
-      console.log(data);
-      Message.getConversation('a@user.com', 'b@user.com', (err, conversation) => {
-        console.log('conversation results = '+conversation);
-        socket.emit('refreshMessages', conversation);
-      })
+    socket.emit('new', {new: 'new'});
+    socket.on('get message', (messageObj, callback) => {
+      console.log(messageObj.senderEmail+' and '+messageObj.recipientEmail);
+      if (messageObj.senderEmail && messageObj.recipientEmail) {
+        socketIDs[messageObj.senderEmail] = socket;
+        Message.getConversation(messageObj.senderEmail, messageObj.recipientEmail, (err, conversation) => {
+          console.log(conversation);
+          callback(err, conversation);
+        });
+      } else {
+        console.log('fail');
+        callback('Expecting senderEmail and recipientEmail', null);
+      }
     });
-    /*
-    // On conversation entry, join broadcast channel
-    socket.on('enter conversation', (conversation) => {
-      socket.join(conversation);
-      // console.log('joined ' + conversation);
+
+    socket.on('add message',(messageObj, callback) => {
+      if (messageObj.senderEmail && messageObj.senderEmail && messageObj.content && callback){
+        console.log('in new message: '+ messageObj.content);
+        Message.addMessage(
+          messageObj.senderEmail,
+          messageObj.recipientEmail,
+          messageObj.content,
+          Date.now(),
+          (err, results) => {
+            if (err) {
+              callback(false);
+            } else if (results) {
+              socketIDs[messageObj.senderEmail].emit('refresh message', results);
+              callback(true);
+            } else {
+              socketIDs[messageObj.senderEmail] = socket;
+              const newMessage = new Message(
+                messageObj.senderEmail,
+                messageObj.recipientEmail,
+                messageObj.content,
+                Date.now()
+              );
+              newMessage.saveMessage((err) => {
+                if (err) {
+                  callback(false);
+                } else {
+                  socketIDs[messageObj.senderEmail].emit('refresh message', results);
+                  callback(true);
+                }
+              });
+            }
+          });
+      } else {
+        callback(false);
+      }
     });
 
-    socket.on('leave conversation', (conversation) => {
-      socket.leave(conversation);
-      // console.log('left ' + conversation);
-    })
+    socket.on('get message involving user', (messageObj, callback) => {
+      if (messageObj.userEmail) {
+        Message.getEmailsInvolvingUser(req.params.userEmail, (err, msgObjs) => {
+          if (err){
+            callback('Error with getting emails method', null);
+          } else {
+            callback(err, msgObjs);
+          }
+        });
+      } else {
+        callback('Expecting userEmail', null);
+      }
+    });
 
-    socket.on('new message', (conversation) => {
-      io.sockets.in(conversation).emit('refresh messages', conversation);
-      });
-
-    socket.on('disconnect', () => {
-      //console.log('user disconnected');
-    });*/
   });
 }
