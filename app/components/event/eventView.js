6d7f355788660f82be2fde2cfabdd8d1dbc33ffd
Eric Ewe <eric.ewe@zalora.com>
2017-04-02 02:54:26 +0800

    Fix event page poster rendering

--- app/components/event/eventView.js
+++ app/components/event/eventView.js
@@ -161,7 +161,7 @@ class EventView extends React.Component {
         <div className="row justify-content-center mb-4">
           <div className="col-md-6 col-12 text-center">
           {
-            (this.state.event_poster)
+            (this.state.event.event_poster)
               ? <img className="img-fluid event-poster mb-2" src={`${this.state.event.event_poster}`} alt="event-poster"/>
               : <img className="img-fluid event-poster mb-2" src="../../resources/images/empty-poster-placeholder.png" alt="event-poster"/>
           }
